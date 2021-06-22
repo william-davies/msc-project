4ab4536674bdfb93dc7a6191c8b37da287b5b181
william-davies <daviwt123@gmail.com>
2021-06-22 23:34:40 +0100

    Worked for P1-P6 inclusive. Threw error for P7

--- scratch.py
+++ scratch.py
@@ -9,18 +9,18 @@ import string
 
 # %%
 # filepath = os.path.join('Stress Dataset/0720202421P1_608/Myo/emg-1532114720.csv')
-filepath = os.path.join("Stress Dataset/0720202421P1_608/Empatica_Right_P1/BVP.csv")
-
-data = pd.read_csv(filepath)
-
-data.iloc[1:].plot()
-plt.show()
-
-# %%
-df = pd.DataFrame(np.arange(20 * 4).reshape((20, 4)), columns=list("ABCD"))
+# filepath = os.path.join("Stress Dataset/0720202421P1_608/Empatica_Right_P1/BVP.csv")
+#
+# data = pd.read_csv(filepath)
+#
+# data.iloc[1:].plot()
+# plt.show()
 
 # %%
-start_time = time.time()
+# df = pd.DataFrame(np.arange(20 * 4).reshape((20, 4)), columns=list("ABCD"))
+
+# %%
+# start_time = time.time()
 # P5 = pd.read_excel('Stress Dataset/0726094551P5_609/test.xlsx', sheet_name='Inf', skiprows=1, usecols='C:E')
 # P5_real = pd.read_excel(
 #     "Stress Dataset/0726094551P5_609/0726094551P5.xlsx",
@@ -45,35 +45,35 @@ start_time = time.time()
 #     usecols="C:E",
 # )
 
-unprocessed_data = pd.read_excel(
-    "Stress Dataset/0726094551P5_609/test.xlsx",
-    sheet_name="Inf",
-    nrows=1,
-)
-
-end_time = time.time()
-print("time elapsed: {:.2f}s".format(end_time - start_time))
+# unprocessed_data = pd.read_excel(
+#     "Stress Dataset/0726094551P5_609/test.xlsx",
+#     sheet_name="Inf",
+#     nrows=1,
+# )
+#
+# end_time = time.time()
+# print("time elapsed: {:.2f}s".format(end_time - start_time))
 
 #%%
-with open("Stress Dataset/0726094551P5_609/0726094551P5_treatment_order.txt", "w") as f:
-    for line in np.arange(50):
-        f.write(f"{line}\n")
+# with open("Stress Dataset/0726094551P5_609/0726094551P5_treatment_order.txt", "w") as f:
+#     for line in np.arange(50):
+#         f.write(f"{line}\n")
 # %%
-frames = P5_real["Row/frame"]
-zeros = frames.index[frames == 0]  # idk why but frames at the end are labelled frame 0
-zeros_array = np.array(zeros)
-assert (zeros_array == np.arange(zeros_array[0], zeros_array[-1] + 1)).all()
-
-final_valid_idx = zeros[0]
-frames = frames[:final_valid_idx]
-bvp = P5_real["BVP"][:final_valid_idx]
+# frames = P5_real["Row/frame"]
+# zeros = frames.index[frames == 0]  # idk why but frames at the end are labelled frame 0
+# zeros_array = np.array(zeros)
+# assert (zeros_array == np.arange(zeros_array[0], zeros_array[-1] + 1)).all()
+#
+# final_valid_idx = zeros[0]
+# frames = frames[:final_valid_idx]
+# bvp = P5_real["BVP"][:final_valid_idx]
 
 # %%
-plt.title("BVP vs frame")
-plt.plot(frames, bvp)
-plt.xlabel("Frame")
-plt.ylabel("BVP")
-plt.show()
+# plt.title("BVP vs frame")
+# plt.plot(frames, bvp)
+# plt.xlabel("Frame")
+# plt.ylabel("BVP")
+# plt.show()
 
 # %%
 R1, M2, R3, M4, R5 = (
@@ -109,17 +109,17 @@ DATA_COL_RANGES = [
 ]  # row/frame, bvp, resp.
 # %%
 
-unprocessed_p1_data = pd.read_excel(
-    "Stress Dataset/0726094551P5_609/test.xlsx",
-    sheet_name="InfP1",
-    # usecols="C:E,G:H,J,M:N,P"
-)
-
-unprocessed_p5_data = pd.read_excel(
-    "Stress Dataset/0726094551P5_609/test.xlsx",
-    sheet_name="InfP5",
-    # usecols="C:E,G:H,J,M:N,P"
-)
+# unprocessed_p1_data = pd.read_excel(
+#     "Stress Dataset/0726094551P5_609/test.xlsx",
+#     sheet_name="InfP1",
+#     # usecols="C:E,G:H,J,M:N,P"
+# )
+#
+# unprocessed_p5_data = pd.read_excel(
+#     "Stress Dataset/0726094551P5_609/test.xlsx",
+#     sheet_name="InfP5",
+#     # usecols="C:E,G:H,J,M:N,P"
+# )
 
 # %%
 
@@ -174,7 +174,7 @@ def build_new_header(data, data_col_ranges):
 
 
 def process_excel_dataframe(unprocessed_data):
-    new_header = build_new_header(unprocessed_data, TREATMENT_LABEL_COL_RANGES)
+    new_header = build_new_header(unprocessed_data, DATA_COL_RANGES)
     # ignore first columns about frequency. ignore first row about treatment label.
     processed_data = unprocessed_data.iloc[1:, 2:]
     processed_data.columns = new_header
@@ -197,6 +197,7 @@ def convert_excel_to_csv(participant_dirname):
 
     unprocessed_data = pd.read_excel(
         os.path.join(participant_dirpath, f"{participant_id}.xlsx"),
+        # os.path.join(participant_dirpath, f"test.xlsx"),
         sheet_name="Inf",
     )
 
@@ -207,12 +208,12 @@ def convert_excel_to_csv(participant_dirname):
 
 
 #%%
-convert_excel_to_csv("0726094551P5_609")
+# convert_excel_to_csv("0726094551P5_609")
 
 # %%
-inf_data = pd.read_csv("Stress Dataset/0720202421P1_608/0720202421P1_inf.csv")
+# inf_data = pd.read_csv("Stress Dataset/0720202421P1_608/0720202421P1_inf.csv")
 # %%
-for participant_dirname in participant_dirnames[0:1]:
+for participant_dirname in participant_dirnames[1:]:
     print(participant_dirname)
     convert_excel_to_csv(participant_dirname)
 
