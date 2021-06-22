ab08c8effee527eddf648546475448c180d8b6f6
william-davies <daviwt123@gmail.com>
2021-06-22 14:27:52 +0100

    Save experiment treatment order

--- scratch.py
+++ scratch.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import re
+import time
 
 # %%
 # filepath = os.path.join('Stress Dataset/0720202421P1_608/Myo/emg-1532114720.csv')
@@ -16,14 +18,44 @@ plt.show()
 df = pd.DataFrame(np.arange(20 * 4).reshape((20, 4)), columns=list("ABCD"))
 
 # %%
+start_time = time.time()
 # P5 = pd.read_excel('Stress Dataset/0726094551P5_609/test.xlsx', sheet_name='Inf', skiprows=1, usecols='C:E')
-P5_real = pd.read_excel(
-    "Stress Dataset/0726094551P5_609/0726094551P5.xlsx",
+# P5_real = pd.read_excel(
+#     "Stress Dataset/0726094551P5_609/0726094551P5.xlsx",
+#     sheet_name="Inf",
+#     skiprows=1,
+#     usecols="C:E",
+# )
+
+# P5_first_line = pd.read_excel(
+#     "Stress Dataset/0726094551P5_609/0726094551P5.xlsx",
+#     sheet_name="Inf",
+#     skiprows=lambda x: x > 5,
+#     usecols="C:E",
+# )
+# workbook_filename = "Stress Dataset/0726094551P5_609/0726094551P5.xlsx"
+#
+# workbook = pd.ExcelFile(workbook_filename)
+# P5_first_line = pd.read_excel(
+#     "Stress Dataset/0726094551P5_609/0726094551P5.xlsx",
+#     sheet_name="Inf",
+#     nrows=5,
+#     usecols="C:E",
+# )
+
+P5_first_line = pd.read_excel(
+    "Stress Dataset/0726094551P5_609/test.xlsx",
     sheet_name="Inf",
-    skiprows=1,
-    usecols="C:E",
+    nrows=1,
 )
 
+end_time = time.time()
+print("time elapsed: {:.2f}s".format(end_time - start_time))
+
+#%%
+with open("treatment_order.txt", "w") as f:
+    for col in P5_first_line.columns:
+        f.write(f"{col}\n")
 # %%
 frames = P5_real["Row/frame"]
 zeros = frames.index[frames == 0]  # idk why but frames at the end are labelled frame 0
@@ -40,3 +72,14 @@ plt.plot(frames, bvp)
 plt.xlabel("Frame")
 plt.ylabel("BVP")
 plt.show()
+
+# %%
+participant_dirs = next(os.walk("Stress Dataset"))[1]
+
+participant_id_pattern = "^(\d{10}P\d{1,2})_"
+participant_id_pattern = re.compile(participant_id_pattern)
+
+for participant_dir in participant_dirs:
+    print(participant_dir)
+    participant_id = participant_id_pattern.search(participant_dir).group(1)
+    print(participant_id)
