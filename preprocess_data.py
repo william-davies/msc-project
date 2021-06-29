f0dd7351386da4970a8f2cb8657b59a3c355ab04
william-davies <daviwt123@gmail.com>
2021-06-29 01:37:22 +0100

    Concatenate all examples into one numpy array

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,21 +3,52 @@ import re
 import numpy as np
 import pandas as pd
 
-# participant ID doesn't include lighting setting information
-from utils import split_data_into_treatments, get_final_recorded_idx
+from utils import split_data_into_treatments, get_final_recorded_idx, get_sample_rate
 
-PARTICIPANT_ID_PATTERN = "(\d{10}P\d{1,2})"
-PARTICIPANT_ID_PATTERN = re.compile(PARTICIPANT_ID_PATTERN)
+from constants import (
+    SECONDS_IN_MINUTE,
+    PARTICIPANT_DIRNAMES_WITH_EXCEL,
+    PARTICIPANT_ID_PATTERN,
+)
 
 participant_dirname = "0720202421P1_608"
-framerate = 256
-participant_id = PARTICIPANT_ID_PATTERN.search(participant_dirname).group(1)
-csv_fp = os.path.join(
-    "Stress Dataset", participant_dirname, f"{participant_id}_inf.csv"
-)
-data = pd.read_csv(csv_fp)
 
-list_of_treatment_timeseries = split_data_into_treatments(data)
+
+def preprocess_data(participant_dirname):
+    """
+
+    :param raw_data: dp.DataFrame: data pulled directly from .xlsx
+    :return:
+    """
+    framerate = get_sample_rate(participant_dirname)
+    participant_id = PARTICIPANT_ID_PATTERN.search(participant_dirname).group(1)
+    csv_fp = os.path.join(
+        "Stress Dataset", participant_dirname, f"{participant_id}_inf.csv"
+    )
+    unprocessed_data = pd.read_csv(csv_fp)
+
+    list_of_treatment_timeseries = split_data_into_treatments(unprocessed_data)
+    list_of_filtered_treatment_timeseries = list(
+        map(filter_recorded_measurements, list_of_treatment_timeseries)
+    )
+    list_of_central_3_minutes = list(
+        map(
+            get_central_3_minutes,
+            list_of_filtered_treatment_timeseries,
+            [framerate] * len(list_of_treatment_timeseries),
+        )
+    )
+
+    treatment_windows = [None] * len(list_of_central_3_minutes)
+    window_size = 2 * SECONDS_IN_MINUTE * framerate
+    overlap_size = 1 * SECONDS_IN_MINUTE * framerate
+
+    for idx, treatment_timeseries in enumerate(list_of_central_3_minutes):
+        windows = get_sliding_windows(treatment_timeseries, window_size, overlap_size)
+        treatment_windows[idx] = windows
+
+    dataset = concatenate_windows(treatment_windows)
+    return dataset
 
 
 def filter_recorded_measurements(data):
@@ -30,12 +61,6 @@ def filter_recorded_measurements(data):
     measurements = data.iloc[:, 1]
     final_recorded_idx = get_final_recorded_idx(frames, measurements)
     return data.iloc[: final_recorded_idx + 1]
-
-
-list_of_filtered_treatment_timeseries = list(
-    map(filter_recorded_measurements, list_of_treatment_timeseries)
-)
-SECONDS_IN_MINUTE = 60
 
 
 def get_central_3_minutes(timeseries, framerate):
@@ -54,19 +79,6 @@ def get_central_3_minutes(timeseries, framerate):
     end_in_frames = start_in_frames + three_minutes_in_frames
 
     return timeseries[start_in_frames:end_in_frames]
-
-
-# central_3_minutes = [None] * len(list_of_treatment_timeseries)
-# for idx in range(len(list_of_treatment_timeseries)):
-#     central_3_minutes[]
-
-list_of_central_3_minutes = list(
-    map(
-        get_central_3_minutes,
-        list_of_filtered_treatment_timeseries,
-        [framerate] * len(list_of_treatment_timeseries),
-    )
-)
 
 
 def get_sliding_windows(timeseries, window_size, overlap_size):
@@ -91,15 +103,6 @@ def get_sliding_windows(timeseries, window_size, overlap_size):
     return windows
 
 
-treatment_windows = [None] * len(list_of_central_3_minutes)
-window_size = 2 * SECONDS_IN_MINUTE * framerate
-overlap_size = 1 * SECONDS_IN_MINUTE * framerate
-
-for idx, treatment_timeseries in enumerate(list_of_central_3_minutes):
-    windows = get_sliding_windows(treatment_timeseries, window_size, overlap_size)
-    treatment_windows[idx] = windows
-
-
 def get_total_number_of_windows(list_of_treatment_windows):
     m = 0
     for windows in list_of_treatment_windows:
@@ -107,7 +110,7 @@ def get_total_number_of_windows(list_of_treatment_windows):
     return m
 
 
-def make_dataset(list_of_treatment_windows):
+def concatenate_windows(list_of_treatment_windows):
     """
     Make dataset ready to pass to model.fit.
     :param list_of_treatment_windows: list of lists. outer_list[0] is list of sliding windows for a specific treatment.
@@ -128,7 +131,14 @@ def make_dataset(list_of_treatment_windows):
     return dataset
 
 
-dataset = make_dataset(treatment_windows)
+# %%
+per_participant_preprocessed_data = {}
+for participant_dirname in PARTICIPANT_DIRNAMES_WITH_EXCEL:
+    per_participant_preprocessed_data[participant_dirname] = preprocess_data(
+        participant_dirname
+    )
 
-breakpoint = 1
-breakpoint = 1
+# %%
+all_participants_preprocessed_data = np.concatenate(
+    list(per_participant_preprocessed_data.values())
+)
