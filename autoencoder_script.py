70927a27e2636eebb198cf253f010d0201dc35f0
william-davies <daviwt123@gmail.com>
2021-06-30 19:13:08 +0100

    Make reproducible

--- autoencoder_script.py
+++ autoencoder_script.py
@@ -4,16 +4,15 @@ from sklearn.model_selection import train_test_split
 
 from constants import PARTICIPANT_DIRNAMES_WITH_EXCEL
 
-np.random.seed(42)
+random_state = np.random.RandomState(42)
 
 data = pd.read_csv("Stress Dataset/dataset_two_min_window.csv")
 
 NUM_PARTICIPANTS = len(PARTICIPANT_DIRNAMES_WITH_EXCEL)
 
 validation_size = round(NUM_PARTICIPANTS * 0.3)
-validation_participants = np.random.randint(
+validation_participants = random_state.randint(
     low=0, high=NUM_PARTICIPANTS, size=validation_size
 )
 
 # %%
-train_set = data.filter()
