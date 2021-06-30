523c3e3dc38b8359351f2c41796f7969e4f6e36e
william-davies <daviwt123@gmail.com>
2021-06-30 21:00:55 +0100

    Find train and validation columns

--- autoencoder_script.py
+++ autoencoder_script.py
@@ -1,9 +1,12 @@
+import re
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from constants import PARTICIPANT_DIRNAMES_WITH_EXCEL
+from constants import PARTICIPANT_DIRNAMES_WITH_EXCEL, PARTICIPANT_NUMBER_PATTERN
 
+# %%
 random_state = np.random.RandomState(42)
 
 data = pd.read_csv("Stress Dataset/dataset_two_min_window.csv")
@@ -11,8 +14,36 @@ data = pd.read_csv("Stress Dataset/dataset_two_min_window.csv")
 NUM_PARTICIPANTS = len(PARTICIPANT_DIRNAMES_WITH_EXCEL)
 
 validation_size = round(NUM_PARTICIPANTS * 0.3)
-validation_participants = random_state.randint(
-    low=0, high=NUM_PARTICIPANTS, size=validation_size
+
+
+# %%
+
+
+def get_participant_number(string):
+    participant_number = PARTICIPANT_NUMBER_PATTERN.search(string).group(1)
+    return participant_number
+
+
+PARTICPANT_NUMBERS_WITH_EXCEL = list(
+    map(get_participant_number, PARTICIPANT_DIRNAMES_WITH_EXCEL)
 )
 
+# %%
+validation_participants = random_state.choice(
+    a=PARTICPANT_NUMBERS_WITH_EXCEL, size=validation_size, replace=False
+)
+validation_participants = set(validation_participants)
+
+print(validation_participants)
+
+# %%
+train_columns = []
+val_columns = []
+for participant_column in data.columns:
+    number_pattern = re.compile("^P(\d{1,2})_")
+    participant_number = number_pattern.match(participant_column).group(1)
+    if participant_number in validation_participants:
+        val_columns.append(participant_column)
+    else:
+        train_columns.append(participant_column)
 # %%
