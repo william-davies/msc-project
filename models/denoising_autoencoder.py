ee799ef15bd980a9c030730d14c2e204a8bb3b09
william-davies <daviwt123@gmail.com>
2021-07-06 16:14:23 +0100

    Implement create_model

--- models/denoising_autoencoder.py
+++ models/denoising_autoencoder.py
@@ -34,3 +34,37 @@ class MLPDenoisingAutoEncoder(Model):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
+
+
+def create_model(config):
+    """
+
+    :param config: wandb.config:
+    :return:
+    """
+    autoencoder = tf.keras.Sequential(
+        [
+            # encoder
+            layers.Dense(
+                config.encoder_1,
+                activation=config.encoder_activation_1,
+                input_shape=(config.timeseries_length,),
+            ),
+            layers.Dense(config.encoder_2, activation=config.encoder_activation_2),
+            layers.Dense(config.encoder_3, activation=config.encoder_activation_3),
+            # decoder
+            layers.Dense(
+                config.decoder_1,
+                activation=config.decoder_activation_1,
+                input_shape=(config.encoder_3,),
+            ),
+            layers.Dense(config.decoder_2, activation=config.decoder_activation_2),
+            layers.Dense(config.decoder_3, activation=config.decoder_activation_3),
+        ]
+    )
+
+    autoencoder.compile(
+        optimizer=config.optimizer, loss=config.loss, metrics=[config.metric]
+    )
+
+    return autoencoder
