793a6c638f5b05b8653778fc167923c8b5863825
william-davies <daviwt123@gmail.com>
2021-07-06 16:46:24 +0100

    Delete subclassed model. It's less compatible with  wandb model saving

--- models/denoising_autoencoder.py
+++ models/denoising_autoencoder.py
@@ -3,40 +3,7 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 
-class MLPDenoisingAutoEncoder(Model):
-    def __init__(self, config):
-        super(MLPDenoisingAutoEncoder, self).__init__()
-        self.encoder = tf.keras.Sequential(
-            [
-                layers.Dense(
-                    config.encoder_1,
-                    activation=config.encoder_activation_1,
-                    input_shape=(config.timeseries_length,),
-                ),
-                layers.Dense(config.encoder_2, activation=config.encoder_activation_2),
-                layers.Dense(config.encoder_3, activation=config.encoder_activation_3),
-            ]
-        )
-
-        self.decoder = tf.keras.Sequential(
-            [
-                layers.Dense(
-                    config.decoder_1,
-                    activation=config.decoder_activation_1,
-                    input_shape=(config.encoder_3,),
-                ),
-                layers.Dense(config.decoder_2, activation=config.decoder_activation_2),
-                layers.Dense(config.decoder_3, activation=config.decoder_activation_3),
-            ]
-        )
-
-    def call(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return decoded
-
-
-def create_model(config):
+def create_autoencoder(config):
     """
 
     :param config: wandb.config:
