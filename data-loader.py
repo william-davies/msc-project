bd55e0b1a536aad0c00145dedac372893c3fc760
william-davies <daviwt123@gmail.com>
2021-07-07 18:15:30 +0100

    Change remove unnamed columns to remove NaN

--- data-loader.py
+++ data-loader.py
@@ -6,6 +6,7 @@ import numpy as np
 import re
 import time
 import string
+import math
 
 # %%
 R1, M2, R3, M4, R5 = (
@@ -43,91 +44,83 @@ DATA_COL_RANGES = [
 # %%
 
 
-def build_new_header(data, data_col_ranges):
-    """
-    :param data: DataFrame
-    :param data_col_ranges:
-    :return:
-    """
-
-    def get_label_from_range(all_column_values, range):
-        """
-        Get treatment label from int range.
-        :param all_column_values: np.array: DataFrame columns
-        :param range: np.array: columns indices
-        :return:
-        """
-        columns_in_range = all_column_values[range]
-        named_columns = remove_unnamed_columns(columns_in_range)
-        label = " ".join(named_columns)
-        label = label.lower()
-        return label.replace(" ", "_")
-
-    def remove_unnamed_columns(columns):
-        """
-        e.g.
-        input: ['Infinity R5', 'Unnamed: 15', 'Unnamed: 16']
-        output: ['Infinity R5']
-        :param columns:
-        :return:
-        """
-        filtered_columns = []
-        UNNAMED_COL_PATTERN = "^Unnamed: \d{1,2}$"
-        UNNAMED_COL_PATTERN = re.compile(UNNAMED_COL_PATTERN)
-
-        for col in columns:
-            if not UNNAMED_COL_PATTERN.search(col):
-                filtered_columns.append(col)
-        return filtered_columns
-
-    new_header = [""] * 3 * len(data_col_ranges)
-
-    column_values = data.columns.values
-    for idx, data_col_range in enumerate(data_col_ranges):
-        treatment_label = get_label_from_range(column_values, data_col_range)
-        new_header[3 * idx] = f"{treatment_label}_frame"
-        new_header[3 * idx + 1] = f"{treatment_label}_bvp"
-        new_header[3 * idx + 2] = f"{treatment_label}_resp"
-
-    return new_header
-
-
-def process_excel_dataframe(unprocessed_data):
-    new_header = build_new_header(unprocessed_data, DATA_COL_RANGES)
-    # ignore first columns about frequency. ignore first row about treatment label.
-    processed_data = unprocessed_data.iloc[1:, 2:]
-    processed_data.columns = new_header
-    return processed_data
+# def build_new_header(data, data_col_ranges):
+#     """
+#     :param data: DataFrame
+#     :param data_col_ranges:
+#     :return:
+#     """
+#
+#     def get_label_from_range(all_column_values, range):
+#         """
+#         Get treatment label from int range.
+#         :param all_column_values: np.array: DataFrame columns
+#         :param range: np.array: columns indices
+#         :return:
+#         """
+#         columns_in_range = all_column_values[range]
+#         named_columns = remove_unnamed_columns(columns_in_range)
+#         label = " ".join(named_columns)
+#         label = label.lower()
+#         return label.replace(" ", "_")
+#
+#     def remove_unnamed_columns(columns):
+#         """
+#         e.g.
+#         input: ['Infinity R5', 'Unnamed: 15', 'Unnamed: 16']
+#         output: ['Infinity R5']
+#         :param columns:
+#         :return:
+#         """
+#         filtered_columns = []
+#         UNNAMED_COL_PATTERN = "^Unnamed: \d{1,2}$"
+#         UNNAMED_COL_PATTERN = re.compile(UNNAMED_COL_PATTERN)
+#
+#         for col in columns:
+#             if not UNNAMED_COL_PATTERN.search(col):
+#                 filtered_columns.append(col)
+#         return filtered_columns
+#
+#     new_header = [""] * 3 * len(data_col_ranges)
+#
+#     column_values = data.columns.values
+#     for idx, data_col_range in enumerate(data_col_ranges):
+#         treatment_label = get_label_from_range(column_values, data_col_range)
+#         new_header[3 * idx] = f"{treatment_label}_frame"
+#         new_header[3 * idx + 1] = f"{treatment_label}_bvp"
+#         new_header[3 * idx + 2] = f"{treatment_label}_resp"
+#
+#     return new_header
 
 
 # %%
 
 
-def convert_excel_to_csv(participant_dirname):
-    """
-    Convert .xlsx to .csv and save.
-
-    :param participant_dirname: name of directory that contains all physiological data on participant
-    :return: None.
-    """
-    participant_dirpath = os.path.join("Stress Dataset", participant_dirname)
-    participant_id = PARTICIPANT_ID_PATTERN.search(participant_dirname).group(1)
-    print(participant_id)
-
-    unprocessed_data = pd.read_excel(
-        # os.path.join(participant_dirpath, f"{participant_dirname}.xlsx"),  # annoying just P10 follows this naming scheme
-        os.path.join(participant_dirpath, f"{participant_id}.xlsx"),
-        sheet_name="Inf",
-    )
-
-    processed_data = process_excel_dataframe(unprocessed_data)
-
-    csv_filepath = os.path.join(participant_dirpath, f"{participant_id}_inf.csv")
-    processed_data.to_csv(csv_filepath, index=False)
+# def convert_excel_to_csv(participant_dirname):
+#     """
+#     Convert .xlsx to .csv and save.
+#
+#     :param participant_dirname: name of directory that contains all physiological data on participant
+#     :return: None.
+#     """
+#     participant_dirpath = os.path.join("Stress Dataset", participant_dirname)
+#     participant_id = PARTICIPANT_ID_PATTERN.search(participant_dirname).group(1)
+#     print(participant_id)
+#
+#     unprocessed_data = pd.read_excel(
+#         # os.path.join(participant_dirpath, f"{participant_dirname}.xlsx"),  # annoying just P10 follows this naming scheme
+#         os.path.join(participant_dirpath, f"{participant_id}.xlsx"),
+#         sheet_name="Inf",
+#     )
+#
+#     processed_data = process_excel_dataframe(unprocessed_data)
+#
+#     csv_filepath = os.path.join(participant_dirpath, f"{participant_id}_inf.csv")
+#     processed_data.to_csv(csv_filepath, index=False)
 
 
 # %%
-def remove_unnamed_columns(columns):
+def remove_nans(columns):
     """
     e.g.
     input: ['Infinity R5', 'Unnamed: 15', 'Unnamed: 16']
@@ -136,17 +129,15 @@ def remove_unnamed_columns(columns):
     :return:
     """
     filtered_columns = []
-    UNNAMED_COL_PATTERN = "^Unnamed: \d{1,2}$"
-    UNNAMED_COL_PATTERN = re.compile(UNNAMED_COL_PATTERN)
-
     for col in columns:
-        if not UNNAMED_COL_PATTERN.search(col):
+        if not math.isnan(col):
             filtered_columns.append(col)
     return filtered_columns
 
 
 class ExcelToCSVConverter:
     SHEET_NAMES = ["Inf", "EmRBVP"]
+    NUM_TREATMENTS = 5
 
     def read_excel(self, participant_dirname):
         """
@@ -171,45 +162,77 @@ class ExcelToCSVConverter:
 
         return read_excel
 
-    def convert_excel_to_csv(self, participant_dirname):
+    def convert_excel_to_csvs(self, participant_dirname):
         """
-        Convert .xlsx to .csv and save.
+        Convert .xlsx to .csv's and save. One .csv for each sheet.
 
         :param participant_dirname: name of directory that contains all physiological data on participant
         :return: None.
         """
         excel_sheets = self.read_excel(participant_dirname)
+        participant_id = PARTICIPANT_ID_PATTERN.search(participant_dirname).group(1)
 
-        processed_data = process_excel_dataframe(excel_sheets)
+        for sheet_name, sheet_data in excel_sheets.items():
+            processed_sheet = self.process_excel_sheet(sheet_data)
 
-        csv_filepath = os.path.join(participant_dirpath, f"{participant_id}_inf.csv")
-        processed_data.to_csv(csv_filepath, index=False)
+            csv_filepath = os.path.join(
+                participant_dirpath,
+                "preprocessed_csvs",
+                f"{participant_id}_{sheet_name}.csv",
+            )
+            processed_sheet.to_csv(csv_filepath, index=False)
 
-    def build_new_header(self, data, data_col_ranges):
+    def process_excel_sheet(self, sheet):
+        new_header = self.build_new_header(sheet, DATA_COL_RANGES)
+        # ignore first columns about frequency. ignore first row about treatment label.
+        processed_data = sheet.iloc[1:, 2:]
+        processed_data.columns = new_header
+        return processed_data
+
+    def build_new_header(self, sheet, data_col_ranges):
         """
-        :param data: DataFrame
+        :param sheet: pd.DataFrame
         :param data_col_ranges:
         :return:
         """
 
-        def get_label_from_range(all_column_values, range):
+        def get_treatment_label(sheet, frame_cols, treatment_idx):
             """
             Get treatment label from int range.
-            :param all_column_values: np.array: DataFrame columns
+            :param sheet: np.array: DataFrame columns
             :param range: np.array: columns indices
             :return:
             """
-            columns_in_range = all_column_values[range]
-            named_columns = remove_unnamed_columns(columns_in_range)
+            relevant_columns = sheet.iloc[
+                0, frame_cols[treatment_idx] : frame_cols[treatment_idx + 1]
+            ]
+            named_columns = remove_nans(relevant_columns)
             label = " ".join(named_columns)
             label = label.lower()
             return label.replace(" ", "_")
 
+        def get_frame_cols(sheet):
+            frame_cols = sheet.iloc[1].values == "Row/frame"
+            frame_cols = frame_cols.nonzero()[0]
+
+            assert len(frame_cols) == self.NUM_TREATMENTS
+            cols_per_treatment = frame_cols[1] - frame_cols[0]
+            correct_frame_cols = np.arange(
+                frame_cols[0],
+                frame_cols[0] + cols_per_treatment * NUM_TREATMENTS,
+                cols_per_treatment,
+            )
+            assert np.array_equal(frame_cols, correct_frame_cols)
+
+            return frame_cols
+
+        frame_cols = get_frame_cols(sheet)
+
         new_header = [""] * 3 * len(data_col_ranges)
 
-        column_values = data.columns.values
+        column_values = sheet.columns.values
         for idx, data_col_range in enumerate(data_col_ranges):
-            treatment_label = get_label_from_range(column_values, data_col_range)
+            treatment_label = get_treatment_label(column_values, data_col_range)
             new_header[3 * idx] = f"{treatment_label}_frame"
             new_header[3 * idx + 1] = f"{treatment_label}_bvp"
             new_header[3 * idx + 2] = f"{treatment_label}_resp"
@@ -279,6 +302,8 @@ correct_frame_cols = np.arange(
 )
 assert np.array_equal(frame_cols, correct_frame_cols)
 
+# %%
+labels = EmRBVP_sheet.iloc[0, frame_cols[0] : frame_cols[1]].values
 # %%
 data_col_ranges = [
     np.arange(frame_col, frame_col + cols_per_treatment) for frame_col in frame_cols
