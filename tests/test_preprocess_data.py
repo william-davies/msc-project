dd8e839da741a232b16834665c376d6f7acec55b
william-davies <daviwt123@gmail.com>
2021-07-26 16:38:37 +0100

    Use fixtures to resuse code

--- tests/test_preprocess_data.py
+++ tests/test_preprocess_data.py
@@ -9,11 +9,8 @@ from msc_project.scripts.preprocess_data import DatasetWrapper
 
 
 class TestGetNoisyMask:
-    def test_all_clean(self):
-        """
-        Every measurement is clean.
-        :return:
-        """
+    @pytest.fixture
+    def dataset_wrapper(self):
         window_size = 10
         step_size = 1
         downsampled_sampling_rate = 16
@@ -23,21 +20,37 @@ class TestGetNoisyMask:
             step_size=step_size,
             downsampled_sampling_rate=downsampled_sampling_rate,
         )
+        return dataset_wrapper
 
+    @pytest.fixture
+    def signal(self, dataset_wrapper):
         index = np.arange(0, 2880)
-        index = pd.TimedeltaIndex(data=index / downsampled_sampling_rate, unit="second")
+        index = pd.TimedeltaIndex(
+            data=index / dataset_wrapper.downsampled_sampling_rate, unit="second"
+        )
         signal_name = "signal_name"
         signal = pd.Series(index=index, data=42, name=signal_name)
+        return signal
 
+    def test_all_clean(self, dataset_wrapper, signal):
+        """
+        Every measurement is clean.
+        :return:
+        """
         noisy_mask = dataset_wrapper.get_noisy_mask(
             participant_number=1, treatment_idx=TREATMENT_INDEXES[0], signal=signal
         )
-        true_noisy_mask = pd.Series(index=index, data=False, name=signal_name)
+        true_noisy_mask = pd.Series(index=signal.index, data=False, name=signal.name)
 
         pd.testing.assert_series_equal(noisy_mask, true_noisy_mask, check_index=True)
 
-    def test_one_noisy_span(self):
-        pass
+    def test_one_noisy_span(self, dataset_wrapper, signal):
+        noisy_mask = dataset_wrapper.get_noisy_mask(
+            participant_number=3, treatment_idx=TREATMENT_INDEXES[2], signal=signal
+        )
+        true_noisy_mask = pd.Series(index=signal.index, data=False, name=signal.name)
+
+        pd.testing.assert_series_equal(noisy_mask, true_noisy_mask, check_index=True)
 
     def test_multiple_noisy_spans(self):
         pass
