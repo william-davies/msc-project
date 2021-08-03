d8c52787c96d8ac6f1519fa4422ac84ddd6de2c2
william-davies <daviwt123@gmail.com>
2021-08-03 14:46:43 +0100

    Re-zero timedeltaindex

--- msc_project/scripts/get_windows.py
+++ msc_project/scripts/get_windows.py
@@ -18,6 +18,7 @@ def get_central_3_minutes(df):
     start = pd.Timedelta(value=1, unit="minute")
     end = pd.Timedelta(value=4, unit="minute")
     central = df[start:end]
+    central.index = central.index - start
     return central
 
 
@@ -34,7 +35,7 @@ def get_window(treatment_series):
     pass
 
 
-def get_noisy_mask(treatment_df):
+def get_treatment_noisy_mask(treatment_df):
     participant_dirname = treatment_df.columns.get_level_values("participant").values[0]
     participant_number = PARTICIPANT_DIRNAME_PATTERN.search(participant_dirname).group(
         PARTICIPANT_NUMBER_GROUP_IDX
@@ -44,15 +45,12 @@ def get_noisy_mask(treatment_df):
     noisy_spans = get_noisy_spans(participant_number, treatment_position)
 
     noisy_mask = pd.Series(data=False, index=treatment_df.index, dtype=bool)
-
     for span in noisy_spans:
         start = pd.Timedelta(value=span.start, unit="second")
         end = pd.Timedelta(value=span.end, unit="second")
         noisy_mask[start:end] = True
 
     return noisy_mask
-
-    brekapoint = 1
 
 
 noisy_mask = pd.DataFrame(
@@ -65,5 +63,15 @@ for participant, participant_df in central_3_minutes.groupby(
     for treatment, treatment_df in participant_df.groupby(
         axis=1, level="treatment_label"
     ):
-        treatment_noisy_mask = get_noisy_mask(treatment_df)
+        treatment_noisy_mask = get_treatment_noisy_mask(treatment_df)
         noisy_mask[participant][treatment]["bvp"] = treatment_noisy_mask.values
+
+# tests
+signal = "bvp"
+index = np.arange()
+
+participant = "0720202421P1_608"
+treatment = "m2_easy"
+p1_r1 = 1
+
+mask = noisy_mask[participant][treatment]["bvp"]
