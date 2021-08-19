8217cf5525ee9a6defeb528baff988090ddb7e91
william-davies <daviwt123@gmail.com>
2021-08-19 15:37:38 +0100

    Make helper function to save reconstructed signals

--- msc_project/scripts/evaluate_autoencoder.py
+++ msc_project/scripts/evaluate_autoencoder.py
@@ -24,14 +24,6 @@ from msc_project.scripts.get_preprocessed_data import get_freq, plot_n_signals
 
 
 # %%
-
-upload_plots_to_wandb: bool = True
-
-run = wandb.init(
-    project=DENOISING_AUTOENCODER_PROJECT_NAME, job_type="model_evaluation"
-)
-
-
 def read_data_split_into_memory(model_artifact):
     """
     Programmatically get the data split artifact used in model training.
@@ -69,28 +61,22 @@ def read_artifacts_into_memory(model_version: int):
     return autoencoder, data_split
 
 
-autoencoder, (train, val, noisy) = read_artifacts_into_memory(model_version=6)
-
-
-# %%
-def get_reconstructed_df(dataframe: pd.DataFrame) -> pd.DataFrame:
-    reconstructed_values = tf.stop_gradient(autoencoder(dataframe.to_numpy()))
-    reconstructed_df = dataframe.copy()
+def get_reconstructed_df(original_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Reconstruct original data.
+    :param original_data:
+    :return:
+    """
+    reconstructed_values = tf.stop_gradient(autoencoder(original_data.to_numpy()))
+    reconstructed_df = original_data.copy()
     reconstructed_df.iloc[:, :] = reconstructed_values.numpy()
     return reconstructed_df
 
 
-reconstructed_train = get_reconstructed_df(train)
-reconstructed_val = get_reconstructed_df(val)
-reconstructed_noisy = get_reconstructed_df(noisy)
-
-
-# %%
 def plot_examples(
     original_data: pd.DataFrame,
     reconstructed_data: np.typing.ArrayLike,
     example_type: str,
-    model_name: str,
     run_name: str,
     save: bool,
     num_examples: int = 5,
@@ -98,11 +84,10 @@ def plot_examples(
     exist_ok: bool = False,
 ) -> str:
     """
-
+    Plot original signal(s) and reconstructed signal(s) on same figure. 1 original/reconstructed signal pair per figure.
     :param original_data:
     :param reconstructed_data:
     :param example_type: Train/Validation/Noisy
-    :param model_name:
     :param save:
     :param num_examples:
     :param example_idxs:
@@ -158,6 +143,44 @@ def plot_examples(
         return ""
 
 
+def save_reconstructed_signals(
+    reconstructed_train, reconstructed_val, reconstructed_noisy
+):
+    """
+    Save as .pkl.
+    :param reconstructed_train:
+    :param reconstructed_val:
+    :param reconstructed_noisy:
+    :return:
+    """
+    reconstructed_dir = os.path.join(ARTIFACTS_ROOT, "reconstructed", run.name)
+    os.mkdir(reconstructed_dir)
+    reconstructed_train.to_pickle(
+        os.path.join(reconstructed_dir, "reconstructed_train.pkl")
+    )
+    reconstructed_val.to_pickle(
+        os.path.join(reconstructed_dir, "reconstructed_val.pkl")
+    )
+    reconstructed_noisy.to_pickle(
+        os.path.join(reconstructed_dir, "reconstructed_noisy.pkl")
+    )
+
+
+# %%
+upload_plots_to_wandb: bool = True
+
+run = wandb.init(
+    project=DENOISING_AUTOENCODER_PROJECT_NAME, job_type="model_evaluation"
+)
+
+autoencoder, (train, val, noisy) = read_artifacts_into_memory(model_version=6)
+
+
+# %%
+reconstructed_train = get_reconstructed_df(train)
+reconstructed_val = get_reconstructed_df(val)
+reconstructed_noisy = get_reconstructed_df(noisy)
+
 # %%
 # run_plots_dir = plot_examples(
 #     original_data=train,
@@ -170,7 +193,7 @@ def plot_examples(
 #     example_idxs=np.arange(0, len(train), 80),
 #     exist_ok=True,
 # )
-# %%
+
 # run_plots_dir = plot_examples(
 #     original_data=val,
 #     reconstructed_data=reconstructed_val.to_numpy(),
@@ -183,8 +206,6 @@ def plot_examples(
 #     exist_ok=True,
 # )
 
-
-# %%
 # run_plots_dir = plot_examples(
 #     original_data=noisy,
 #     reconstructed_data=reconstructed_noisy.to_numpy(),
@@ -198,41 +219,14 @@ def plot_examples(
 # )
 
 # %%
-reconstructed_dir = os.path.join(ARTIFACTS_ROOT, "reconstructed", run.name)
-os.mkdir(reconstructed_dir)
-reconstructed_train.to_pickle(
-    os.path.join(reconstructed_dir, "reconstructed_train.pkl")
-)
-reconstructed_val.to_pickle(os.path.join(reconstructed_dir, "reconstructed_val.pkl"))
-reconstructed_noisy.to_pickle(
-    os.path.join(reconstructed_dir, "reconstructed_noisy.pkl")
-)
 
+save_reconstructed_signals(reconstructed_train, reconstructed_val, reconstructed_noisy)
 # %%
 
 plt.close("all")
 random_idx = np.random.randint(low=0, high=len(train) + 1)
 random_example = train.iloc[random_idx]
-bvp_peaks, _ = scipy.signal.find_peaks(random_example, height=0.2)
-bvp_peak_times = random_example.index.total_seconds()[bvp_peaks]
-period_lengths = bvp_peak_times[1:] - bvp_peak_times[:-1]
-# %%
-plt.figure()
-plot_n_signals(
-    signals=[
-        (random_example, ""),
-    ]
-)
-plt.vlines(bvp_peak_times, ymin=0, ymax=1)
-plt.show()
 
-
-# %%
-plt.figure()
-plt.title("Histogram of period freq")
-plt.xlabel("period freq (Hz)")
-plt.ylabel("count")
-plt.hist(1 / period_lengths)
 
 # %%
 fs = get_freq(random_example.index)
@@ -252,26 +246,6 @@ plt.show()
 # expected range of heart rate (Hz)
 SQI_HR_range_min = 0.8
 SQI_HR_range_max = 2
-
-train = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artefacts/data_split/train.pkl"
-)
-val = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artefacts/data_split/val.pkl"
-)
-noisy = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artefacts/data_split/noisy.pkl"
-)
-
-reconstructed_train = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artefacts/reconstructed/deep-wind-128/reconstructed_train.pkl"
-)
-reconstructed_val = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artefacts/reconstructed/deep-wind-128/reconstructed_val.pkl"
-)
-reconstructed_noisy = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artefacts/reconstructed/deep-wind-128/reconstructed_noisy.pkl"
-)
 
 
 def get_SQI(
