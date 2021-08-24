061fae2a3e45215195bd07936278654cdd4f48b5
william-davies <daviwt123@gmail.com>
2021-08-24 18:02:28 +0100

    Start attempt with return_state=True

--- msc_project/models/lstm_autoencoder.py
+++ msc_project/models/lstm_autoencoder.py
@@ -5,29 +5,62 @@ from tensorflow.keras.utils import plot_model
 from tensorflow.keras.layers import RepeatVector
 
 
+# def create_autoencoder(config: Dict):
+#     """
+#
+#     :param config:
+#     :return:
+#     """
+#     bottleneck_size = 8
+#     autoencoder = tf.keras.Sequential(
+#         [
+#             # encoder
+#             keras.layers.LSTM(
+#                 units=bottleneck_size,
+#                 activation="tanh",
+#                 input_shape=(config["timeseries_length"], 1),
+#             ),
+#             RepeatVector(config["timeseries_length"]),
+#             # decoder
+#             keras.layers.LSTM(
+#                 units=bottleneck_size, activation="tanh", return_sequences=True
+#             ),
+#             keras.layers.TimeDistributed(keras.layers.Dense(1, activation="sigmoid")),
+#         ]
+#     )
+#
+#     autoencoder.compile(
+#         optimizer=config["optimizer"], loss=config["loss"], metrics=[config["metric"]]
+#     )
+#
+#     return autoencoder
+
+
 def create_autoencoder(config: Dict):
     """
 
     :param config:
     :return:
     """
+    num_features = 1
     bottleneck_size = 8
-    autoencoder = tf.keras.Sequential(
-        [
-            # encoder
-            keras.layers.LSTM(
-                units=bottleneck_size,
-                activation="tanh",
-                input_shape=(config["timeseries_length"], 1),
-            ),
-            RepeatVector(config["timeseries_length"]),
-            # decoder
-            keras.layers.LSTM(
-                units=bottleneck_size, activation="tanh", return_sequences=True
-            ),
-            keras.layers.TimeDistributed(keras.layers.Dense(1, activation="sigmoid")),
-        ]
+
+    # encoder
+    encoder_inputs = keras.Input(shape=(config["timeseries_length"], num_features))
+    encoder = keras.Bidirectional(keras.LSTM(bottleneck_size, return_state=True))
+    encoder_outputs, forward_h, forward_c, backward_h, backward_c = encoder(
+        encoder_inputs
     )
+    state_h = keras.Concatenate()([forward_h, backward_h])
+    state_c = keras.Concatenate()([forward_c, backward_c])
+    encoder_states = [state_h, state_c]
+
+    repeat_vector = RepeatVector(config["timeseries_length"])(encoder_outputs)
+
+    # decoder
+    decoder_inputs = keras.Input(shape=(config["timeseries_length"], num_features))
+    decoder_lstm = keras.LSTM(bottleneck_size * 2, return_sequences=True)
+    decoder_outputs = decoder_lstm(decoder_inputs, initial_state=encoder_states)
 
     autoencoder.compile(
         optimizer=config["optimizer"], loss=config["loss"], metrics=[config["metric"]]
