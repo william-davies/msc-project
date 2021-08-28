e4df66e9f9fd884995d15c68885d081bc11fb7c5
william-davies <daviwt123@gmail.com>
2021-08-28 01:41:27 +0100

    CNN try more filters

--- msc_project/models/cnn_autoencoder.py
+++ msc_project/models/cnn_autoencoder.py
@@ -13,7 +13,8 @@ from tensorflow.keras.layers import (
     UpSampling1D,
 )
 from keras import Input
-from lstm_autoencoder import reshape_data
+
+from msc_project.models.lstm_autoencoder import reshape_data
 
 
 def create_autoencoder(config: Dict):
@@ -22,19 +23,20 @@ def create_autoencoder(config: Dict):
     :param config:
     :return:
     """
+    timesteps = config["timeseries_length"]
     num_features = 1
 
-    input_signal = Input(shape=(config["timeseries_length"], num_features))
-    x = Conv1D(16, 5, activation="relu", padding="same")(input_signal)
-    x = MaxPool1D(4, padding="same")(x)
-    x = Conv1D(1, 3, activation="relu", padding="same")(x)
-    encoded = MaxPool1D(4, padding="same", name="final_encoder_layer")(x)
+    input_signal = Input(shape=(timesteps, num_features))
+    x = Conv1D(8, 16, activation="relu", padding="same")(input_signal)
+    x = MaxPool1D(2, padding="same")(x)
+    x = Conv1D(8, 4, activation="relu", padding="same")(x)
+    x = MaxPool1D(2, padding="same")(x)
 
-    x = Conv1D(1, 3, activation="relu", padding="same")(encoded)
-    x = UpSampling1D(4)(x)
-    x = Conv1D(16, 2, activation="relu", padding="same")(x)
-    x = UpSampling1D(4)(x)
-    decoded = Conv1D(1, 3, activation="sigmoid", padding="same")(x)
+    x = Conv1D(8, 4, activation="relu", padding="same", name="decoder-1")(x)
+    x = UpSampling1D(2)(x)
+    x = Conv1D(8, 4, activation="relu", padding="same")(x)
+    x = UpSampling1D(2)(x)
+    decoded = Conv1D(1, 16, activation="sigmoid", padding="same")(x)
     autoencoder = keras.Model(input_signal, decoded)
 
     autoencoder.compile(
