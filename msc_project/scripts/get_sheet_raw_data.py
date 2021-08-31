0d26aab3ec4b94035233aefbc1eafe374f6301ae
william-davies <daviwt123@gmail.com>
2021-08-31 01:57:25 +0100

    Get sample rate for sheet

--- msc_project/scripts/get_sheet_raw_data.py
+++ msc_project/scripts/get_sheet_raw_data.py
@@ -1,10 +1,23 @@
 import pandas as pd
 
-all_data = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/data/Stress Dataset/dataframes/raw_data.pkl"
-)
 
-all_data.xs(key="Inf", axis=1, level="sheet")
-all_data.xs(key=["EmLBVP", "EmRBVP"], axis=1, level="sheet")
+def get_sample_rate(sheet_name: str):
+    all_sample_rates = pd.read_pickle(
+        "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/data/Stress Dataset/dataframes/sample_rates.pkl"
+    )
+    sheet_sample_rates = all_sample_rates[sheet_name]
+    assert (sheet_sample_rates.values[0] == sheet_sample_rates.values).all()
+    return sheet_sample_rates[0]
 
-all_data.loc[:, pd.IndexSlice[:, ["EmLBVP", "EmRBVP"]]]
+
+def get_sheet_data(all_data: pd.DataFrame, sheet_name: str):
+    sheet_data = all_data.xs(key=sheet_name, axis=1, level="sheet")
+
+
+if __name__ == "__main__":
+    all_data = pd.read_pickle(
+        "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/data/Stress Dataset/dataframes/raw_data.pkl"
+    )
+
+    sheet_name = "Inf"
+    sheet_data = get_sheet_data(all_data=all_data, sheet_name=sheet_name)
