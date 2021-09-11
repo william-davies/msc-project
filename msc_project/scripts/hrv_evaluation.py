671f751b5a34cf6f5e5266e979a2373fb38cc881
william-davies <daviwt123@gmail.com>
2021-09-11 18:50:04 +0100

    Compute frequency HRV metrics

--- msc_project/scripts/hrv_evaluation.py
+++ msc_project/scripts/hrv_evaluation.py
@@ -11,9 +11,22 @@ signal = all_data["0720202421P1_608", "r1", "bvp"]
 
 sample_rate = get_freq(signal.index)
 
-wd, m = hp.process(signal, sample_rate, report_time=True)
+wd, m = hp.process(signal, sample_rate, report_time=True, calc_freq=True)
 
-plt.figure(figsize=(12, 4))
+hp.plotter(wd, m)
+
+# display measures computed
+for measure in m.keys():
+    print("%s: %f" % (measure, m[measure]))
+
+
+windowed_data = pd.read_pickle(
+    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artifacts/preprocessed_data/inf_preprocessed_datav2/windowed_raw_data.pkl"
+)
+window = windowed_data["0720202421P1_608", "r1", "bvp", 0.0]
+
+wd, m = hp.process(window, sample_rate, report_time=True, calc_freq=True)
+
 hp.plotter(wd, m)
 
 # display measures computed
