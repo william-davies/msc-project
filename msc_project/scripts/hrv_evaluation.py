79a9f98a863ddcf9ae590feb4c49714b62a2cdd3
william-davies <daviwt123@gmail.com>
2021-09-12 17:55:50 +0100

    Inspect problematic signals. Can't replicated BadSignalWarning for some reason

--- msc_project/scripts/hrv_evaluation.py
+++ msc_project/scripts/hrv_evaluation.py
@@ -5,37 +5,18 @@ import numpy as np
 
 from msc_project.scripts.get_preprocessed_data import get_freq
 
-all_data = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artifacts/Inf_raw_data.pkl"
-)
-signal = all_data["0720202421P1_608", "r1", "bvp"]
-
-sample_rate = get_freq(signal.index)
-
-wd, m = hp.process(signal, sample_rate, report_time=True, calc_freq=True)
-
-hp.plotter(wd, m)
-
-# display measures computed
-for measure in m.keys():
-    print("%s: %f" % (measure, m[measure]))
-
-
-windowed_data = pd.read_pickle(
-    "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artifacts/preprocessed_data/inf_preprocessed_datav2/windowed_raw_data.pkl"
-)
-window = windowed_data["0720202421P1_608", "r1", "bvp", 0.0]
-
-wd, m = hp.process(window, sample_rate, report_time=True, calc_freq=True)
-
-hp.plotter(wd, m)
-
-# display measures computed
-for measure in m.keys():
-    print("%s: %f" % (measure, m[measure]))
+# %%
 
 
 def hp_process_wrapper(hrdata, sample_rate, report_time, calc_freq):
+    """
+    rows is time index. wrapper for hp.process. handles BadSignalWarning.
+    :param hrdata:
+    :param sample_rate:
+    :param report_time:
+    :param calc_freq:
+    :return:
+    """
     try:
         wd, m = hp.process(
             hrdata,
@@ -46,31 +27,84 @@ def hp_process_wrapper(hrdata, sample_rate, report_time, calc_freq):
         merged = {**wd, **m}
         return pd.Series(merged)
     except hp.exceptions.BadSignalWarning:
+        print(hrdata.name)
         return None
 
 
-# small_windowed_data = windowed_data.loc[:, (['0720202421P1_608','0725095437P2_608'])]
-small_windowed_data = windowed_data.loc[
-    :, (["0720202421P1_608"], slice(None), slice(None), np.arange(5))
-]
+if __name__ == "__main__":
 
-hrv_metrics = small_windowed_data.apply(
-    func=hp_process_wrapper,
-    axis=0,
-    sample_rate=sample_rate,
-    report_time=False,
-    calc_freq=True,
-)
+    all_data = pd.read_pickle(
+        "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artifacts/Inf_raw_data.pkl"
+    )
+    signal = all_data["0720202421P1_608", "r1", "bvp"]
 
-wd_keys = set(wd.keys())
-m_keys = set(m.keys())
-intersect = wd_keys & m_keys
+    sample_rate = get_freq(signal.index)
 
+    wd, m = hp.process(signal, sample_rate, report_time=True, calc_freq=True)
 
-def get_HRV_metrics(signal_data: pd.DataFrame) -> pd.DataFrame:
-    """
+    hp.plotter(wd, m)
 
-    :param signal_data:
-    :return:
-    """
-    pass
+    # display measures computed
+    for measure in m.keys():
+        print("%s: %f" % (measure, m[measure]))
+
+    windowed_data = pd.read_pickle(
+        "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/msc_project/scripts/wandb_artifacts/preprocessed_data/inf_preprocessed_datav2/windowed_raw_data.pkl"
+    )
+    window = windowed_data["0720202421P1_608", "r1", "bvp", 0.0]
+
+    wd, m = hp.process(window, sample_rate, report_time=True, calc_freq=True)
+
+    hp.plotter(wd, m)
+
+    # display measures computed
+    for measure in m.keys():
+        print("%s: %f" % (measure, m[measure]))
+
+    # small_windowed_data = windowed_data.loc[:, (['0720202421P1_608','0725095437P2_608'])]
+    small_windowed_data = windowed_data.loc[
+        :, (["0720202421P1_608"], slice(None), slice(None), np.arange(20))
+    ]
+    small_windowed_data = windowed_data.loc[
+        :,
+        (
+            ["0720202421P1_608", "0725095437P2_608"],
+            slice(None),
+            slice(None),
+            slice(None),
+        ),
+    ]
+
+    hrv_metrics = small_windowed_data.apply(
+        func=hp_process_wrapper,
+        axis=0,
+        sample_rate=sample_rate,
+        report_time=False,
+        calc_freq=True,
+    )
+
+    is_none = hrv_metrics.isnull().all()
+    is_none_indexes = is_none.index[is_none]
+
+    problematic = small_windowed_data[("0720202421P1_608", "r3", "bvp", 292.0)]
+    wd, m = hp.process(window, sample_rate, report_time=True, calc_freq=True)
+    hp.plotter(wd, m)
+
+    has_none = hrv_metrics["0720202421P1_608", "r3", "bvp", 0.0]
+
+    wd_keys = set(wd.keys())
+    m_keys = set(m.keys())
+    intersect = wd_keys & m_keys
+
+    def get_key_types(mydict):
+        for key in mydict.keys():
+            print(key)
+            print(type(mydict[key]))
+
+    def get_HRV_metrics(signal_data: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param signal_data:
+        :return:
+        """
+        pass
