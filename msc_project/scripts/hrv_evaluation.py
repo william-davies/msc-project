5328ecbaaeda6dd7753ff2f55ffc48e29bce401e
william-davies <daviwt123@gmail.com>
2021-09-12 16:19:47 +0100

    make hp_process_wrapper

--- msc_project/scripts/hrv_evaluation.py
+++ msc_project/scripts/hrv_evaluation.py
@@ -1,6 +1,7 @@
 import heartpy as hp
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from msc_project.scripts.get_preprocessed_data import get_freq
 
@@ -32,3 +33,44 @@ hp.plotter(wd, m)
 # display measures computed
 for measure in m.keys():
     print("%s: %f" % (measure, m[measure]))
+
+
+def hp_process_wrapper(hrdata, sample_rate, report_time, calc_freq):
+    try:
+        wd, m = hp.process(
+            hrdata,
+            sample_rate=sample_rate,
+            report_time=report_time,
+            calc_freq=calc_freq,
+        )
+        merged = {**wd, **m}
+        return pd.Series(merged)
+    except hp.exceptions.BadSignalWarning:
+        return None
+
+
+# small_windowed_data = windowed_data.loc[:, (['0720202421P1_608','0725095437P2_608'])]
+small_windowed_data = windowed_data.loc[
+    :, (["0720202421P1_608"], slice(None), slice(None), np.arange(5))
+]
+
+hrv_metrics = small_windowed_data.apply(
+    func=hp_process_wrapper,
+    axis=0,
+    sample_rate=sample_rate,
+    report_time=False,
+    calc_freq=True,
+)
+
+wd_keys = set(wd.keys())
+m_keys = set(m.keys())
+intersect = wd_keys & m_keys
+
+
+def get_HRV_metrics(signal_data: pd.DataFrame) -> pd.DataFrame:
+    """
+
+    :param signal_data:
+    :return:
+    """
+    pass
