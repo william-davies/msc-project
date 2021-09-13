31d7308859897653a4ead593ad335892c96477b1
william-davies <daviwt123@gmail.com>
2021-09-13 19:09:08 +0100

    First iteration of metrics of interest

--- msc_project/scripts/evaluate_hrv.py
+++ msc_project/scripts/evaluate_hrv.py
@@ -1,25 +1,20 @@
 import os
 import pandas as pd
 
-# metrics_of_interest = ['bpm', 'sdnn']
-metrics_of_interest = ["hf_nu", "hf_perc", "lf_nu", "lf_perc"]
-metrics_of_interest = ["hf_nu", "hf_perc"]
-
+#%%
 
 run_dir = "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/results/hrv/unique-dream-323"
 inf_raw_data_hrv = pd.read_pickle(os.path.join(run_dir, "inf_raw_data_hrv.pkl"))
 
-
-def get_subset_hrv_metrics(hrv_metrics):
-    return hrv_metrics[metrics_of_interest]
-
-
-subset = inf_raw_data_hrv.loc[metrics_of_interest]
-comparison = subset.loc["hf_nu"] != subset.loc["hf_perc"]
-different = comparison[comparison].index
-different_df = subset[different]
-
-notnull = subset.loc[
-    :, (~subset.loc["hf_nu"].isnull() | ~subset.loc["hf_perc"].isnull())
+metrics_of_interest = [
+    "bpm",
+    "ibi",
+    "sdnn",
+    "rmssd",
+    "pnn20",
+    "pnn50",
+    "hf",
+    "sdsd",
+    "hr_mad",
 ]
-(notnull.loc["hf_nu"] != notnull.loc["hf_perc"]).sum()
+subset_hrv = inf_raw_data_hrv.loc[metrics_of_interest]
