f9c7d4e9fb04eb9a7d0c83e697b52af351ee7306
william-davies <daviwt123@gmail.com>
2021-09-14 15:24:41 +0100

    Replace masked with nan

--- msc_project/scripts/hrv/hrv_testing_ground.py
+++ msc_project/scripts/hrv/hrv_testing_ground.py
@@ -1,55 +1,59 @@
 import os
-
-import heartpy as hp
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 import wandb
+import numpy as np
 
+#%%
 from msc_project.constants import DENOISING_AUTOENCODER_PROJECT_NAME, BASE_DIR
-from msc_project.scripts.evaluate_autoencoder import (
-    download_artifact_if_not_already_downloaded,
-    get_model,
-    get_reconstructed_df,
-)
-from msc_project.scripts.get_preprocessed_data import get_freq
-import tensorflow as tf
 
-run_dir = "/results/hrv_rmse/spring-sunset-337"
-rmse_dir = os.path.join(run_dir, "to_upload")
+# run_dir = "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/results/hrv/unique-dream-323"
+# inf_raw_data_heartpy_output = pd.read_pickle(os.path.join(run_dir, "inf_raw_data_hrv.pkl"))
+# empatica_raw_data_heartpy_output = pd.read_pickle(os.path.join(run_dir, "empatica_raw_data_hrv.pkl"))
+from msc_project.scripts.hrv.get_rmse import get_rmse
+
+metrics_of_interest = [
+    "bpm",
+    "ibi",
+    "sdnn",
+    "rmssd",
+    "pnn20",
+    "pnn50",
+    "hf",
+    "sdsd",
+    "hr_mad",
+]
 
 
-def get_rmse(filename: str) -> pd.DataFrame:
-    return pd.read_pickle(os.path.join(rmse_dir, filename))
+def get_hrv_metrics(heartpy_output: pd.DataFrame) -> pd.DataFrame:
+    return heartpy_output.loc[metrics_of_interest]
 
 
-empatica_raw = get_rmse("empatica_raw_data_rmse.pkl")
-empatica_traditional_preprocessed = get_rmse(
-    "empatica_traditional_preprocessed_data_rmse.pkl"
-)
-empatica_intermediate_preprocessed = get_rmse(
-    "empatica_intermediate_preprocessed_data_rmse.pkl"
-)
-empatica_proposed_denoised = get_rmse("empatica_proposed_denoised_data_rmse.pkl")
+# %%
 
-all_rmses = pd.concat(
-    objs=[
-        empatica_raw,
-        empatica_traditional_preprocessed,
-        empatica_intermediate_preprocessed,
-        empatica_proposed_denoised,
-    ],
-    axis=1,
-    keys=[
-        "empatica_raw",
-        "empatica_traditional_preprocessed",
-        "empatica_intermediate_preprocessed",
-        "empatica_proposed_denoised",
-    ],
-)
-min = all_rmses.min(axis=1)
-max = all_rmses.max(axis=1)
-normalized = all_rmses.subtract(min, axis=0).divide(max - min, axis=0)
 
-normalized.plot.bar()
-plt.show()
+def read_hrv_of_interest(filename) -> pd.DataFrame:
+    return replace_masked_with_nan(pd.read_pickle(os.path.join(run_dir, filename)))
+
+
+def replace_masked_with_nan(df: pd.DataFrame) -> pd.DataFrame:
+    cond = df.applymap(lambda element: element is np.ma.masked)
+    replaced = df.mask(cond=cond)
+    return replaced
+
+
+if __name__ == "__main__":
+    run_dir = "/Users/williamdavies/OneDrive - University College London/Documents/MSc Machine Learning/MSc Project/My project/msc_project/results/hrv_rmse/spring-sunset-337"
+
+    inf_raw = read_hrv_of_interest("inf_raw_data_hrv_of_interest.pkl")
+    emp_raw = read_hrv_of_interest("empatica_raw_data_hrv_of_interest.pkl")
+    emp_traditional = read_hrv_of_interest(
+        "empatica_traditional_preprocessed_data_hrv_of_interest.pkl"
+    )
+    emp_intermediate = read_hrv_of_interest(
+        "empatica_intermediate_preprocessed_data_hrv_of_interest.pkl"
+    )
+    emp_proposed = read_hrv_of_interest(
+        "empatica_proposed_denoised_data_hrv_of_interest.pkl"
+    )
+
+    rmse = get_rmse(gt_hrv_metrics=inf_raw, other_hrv_metrics=emp_raw)
