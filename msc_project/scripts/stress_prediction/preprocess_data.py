879a03f069ffc4f1ab6a748e216482e9b8be2ac8
william-davies <daviwt123@gmail.com>
2021-09-15 21:17:42 +0100

    Get proposed denoised data

--- msc_project/scripts/stress_prediction/preprocess_data.py
+++ msc_project/scripts/stress_prediction/preprocess_data.py
@@ -5,6 +5,7 @@ from msc_project.constants import (
     SheetNames,
     PREPROCESSED_DATA_ARTIFACT,
 )
+from msc_project.scripts.evaluate_autoencoder import get_model, get_reconstructed_df
 from msc_project.scripts.get_preprocessed_data import downsample, get_freq
 from msc_project.scripts.hrv.get_hrv import get_artifact_dataframe
 
@@ -17,21 +18,34 @@ if __name__ == "__main__":
     sheet_name = SheetNames.INFINITY.value
     preprocessed_data_artifact_version: int = 2
     downsampled_rate: float = 16
+    model_version: int = 40
 
     autoencoder_preprocessed_data_artifact = run.use_artifact(
         artifact_or_name=f"{DENOISING_AUTOENCODER_PROJECT_NAME}/{sheet_name}_preprocessed_data:v{preprocessed_data_artifact_version}",
         type=PREPROCESSED_DATA_ARTIFACT,
     )
 
-    windowed_raw_data = get_artifact_dataframe(
+    raw_data = get_artifact_dataframe(
         run=run,
         artifact_or_name=autoencoder_preprocessed_data_artifact,
         pkl_filename="inf_raw_data_hrv.pkl",
     )
 
-    original_fs = get_freq(windowed_raw_data.index)
-    downsampled_windowed_raw_data = downsample(
-        original_data=windowed_raw_data,
+    intermediate_preprocessed_data = get_artifact_dataframe(
+        run=run,
+        artifact_or_name=autoencoder_preprocessed_data_artifact,
+        pkl_filename="windowed_intermediate_preprocessed_data.pkl",
+    )
+
+    original_fs = get_freq(raw_data.index)
+    downsampled_raw_data = downsample(
+        original_data=raw_data,
         original_rate=original_fs,
         downsampled_rate=downsampled_rate,
     )
+
+    autoencoder = get_model(run=run, model_version=model_version)
+    proposed_denoised_data = get_reconstructed_df(
+        to_reconstruct=intermediate_preprocessed_data.T,
+        autoencoder=autoencoder,
+    ).T
