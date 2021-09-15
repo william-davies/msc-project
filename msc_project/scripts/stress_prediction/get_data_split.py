c46afc23a5138e51c3db4d4fbe44b21cd7a559bc
william-davies <daviwt123@gmail.com>
2021-09-15 22:41:59 +0100

    Implement handle_data_split function

--- msc_project/scripts/stress_prediction/get_data_split.py
+++ msc_project/scripts/stress_prediction/get_data_split.py
@@ -8,6 +8,7 @@ from msc_project.constants import (
     STRESS_PREDICTION_PROJECT_NAME,
     SheetNames,
     PREPROCESSED_DATA_ARTIFACT,
+    BASE_DIR,
 )
 from msc_project.scripts.get_data_split import DatasetPreparer
 from msc_project.scripts.hrv.get_hrv import get_artifact_dataframe
@@ -27,12 +28,39 @@ def get_autoencoder_preprocessed_data_artifact(
     return preprocessed_data_artifacts[0]
 
 
+def handle_data_split(signals: pd.DataFrame, data_name: str):
+    """
+    Split data and save.
+
+    :param signals:
+    :param data_name: only_downsampled/labels/intermediate_preprocessed etc.
+    :return:
+    """
+    dataset_preparer = DatasetPreparer(
+        noise_tolerance=0,
+        signals=signals,
+        noisy_mask=noisy_mask,
+    )
+    train_signals, val_signals, noisy_signals = dataset_preparer.get_dataset()
+
+    save_dir = os.path.join(run_dir, data_name)
+    train_signals.to_pickle(os.path.join(save_dir, "train.pkl"))
+    val_signals.to_pickle(os.path.join(save_dir, "val.pkl"))
+    noisy_signals.to_pickle(os.path.join(save_dir, "noisy.pkl"))
+    return train_signals, val_signals, noisy_signals
+
+
 if __name__ == "__main__":
     run = wandb.init(
         project=STRESS_PREDICTION_PROJECT_NAME, job_type="data_split", save_code=True
     )
     sheet_name = SheetNames.INFINITY.value
     preprocessed_data_artifact_version = 1
+
+    run_dir = os.path.join(
+        BASE_DIR, "data", "stress_prediction", "data_split", sheet_name, run.name
+    )
+    os.makedirs(run_dir)
 
     stress_prediction_preprocessed_data_artifact = run.use_artifact(
         artifact_or_name=f"{sheet_name}:v{preprocessed_data_artifact_version}",
