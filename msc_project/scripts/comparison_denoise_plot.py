aa321fe099ae9830cfad25d610d466d8cc5cee59
william-davies <daviwt123@gmail.com>
2021-09-19 17:53:58 +0100

    Get raw data

--- msc_project/scripts/comparison_denoise_plot.py
+++ msc_project/scripts/comparison_denoise_plot.py
@@ -1,9 +1,13 @@
 import os
 
+import pandas as pd
 import wandb
 
 from msc_project.constants import SheetNames, DENOISING_AUTOENCODER_PROJECT_NAME
-from msc_project.scripts.evaluate_autoencoder import get_model
+from msc_project.scripts.evaluate_autoencoder import (
+    get_model,
+    download_preprocessed_data,
+)
 from msc_project.scripts.utils import get_artifact_dataframe
 
 
@@ -26,16 +30,29 @@ def get_signal_processed_data(run, data_split_artifact):
     return train, val, noisy
 
 
+def get_raw_data(data_split_artifact, train_index, val_index, noisy_index):
+    preprocessed_data_fp = download_preprocessed_data(data_split_artifact)
+    # transpose so examples is row axis. like train/val/noisy
+    raw_data = pd.read_pickle(
+        os.path.join(preprocessed_data_fp, "windowed_raw_data.pkl")
+    ).T
+    train = raw_data.loc[train_index]
+    val = raw_data.loc[val_index]
+    noisy = raw_data.loc[noisy_index]
+
+    return train, val, noisy
+
+
 if __name__ == "__main__":
     sheet_name_to_evaluate_on = SheetNames.EMPATICA_LEFT_BVP.value
-    data_split_version = 4
+    data_split_version = 2
     model_artifact_name = "trained_on_EmLBVP:v0"
 
     run = wandb.init(
         project=DENOISING_AUTOENCODER_PROJECT_NAME,
         job_type="model_evaluation",
         save_code=True,
-        notes="comparison plot",
+        notes="comparison plots",
     )
 
     autoencoder = get_model(run=run, artifact_or_name=model_artifact_name)
@@ -52,3 +69,10 @@ if __name__ == "__main__":
         signal_processed_val,
         signal_processed_noisy,
     ) = get_signal_processed_data(run=run, data_split_artifact=data_split_artifact)
+
+    raw_train, raw_val, raw_noisy = get_raw_data(
+        data_split_artifact=data_split_artifact,
+        train_index=signal_processed_train.index,
+        val_index=signal_processed_val.index,
+        noisy_index=signal_processed_noisy.index,
+    )
