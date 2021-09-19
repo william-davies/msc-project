0487e43e58f1b9179c2de9917292a7e3dc5ddcca
william-davies <daviwt123@gmail.com>
2021-09-19 21:31:56 +0100

    Plot both signals

--- msc_project/scripts/compare_sqi_of_raw_and_downsample.py
+++ msc_project/scripts/compare_sqi_of_raw_and_downsample.py
@@ -1,10 +1,13 @@
+import numpy as np
 import pandas as pd
 import wandb
+from matplotlib import pyplot as plt
 
 from msc_project.constants import SheetNames, DENOISING_AUTOENCODER_PROJECT_NAME
 from msc_project.scripts.comparison_denoise_plot import (
     get_only_downsampled_data,
     get_raw_data,
+    get_data_split_indexes,
 )
 from msc_project.scripts.evaluate_autoencoder import get_SQI
 
@@ -39,3 +42,30 @@ if __name__ == "__main__":
 
     raw_sqi = get_SQI(data=raw_data)
     downsampled_sqi = get_SQI(data=downsampled)
+
+    train_indexes, val_indexes, noisy_indexes = get_data_split_indexes(
+        run=run, data_split_artifact=data_split_artifact
+    )
+
+    random_noisy_example = np.random.choice(noisy_indexes)
+
+    def plot_line(signal_data, sqi, example, name):
+        plt.plot(
+            signal_data.loc[example].index,
+            signal_data.loc[example],
+            label=f"{name} (SQI={sqi.loc[example].item()})",
+        )
+
+    plt.figure()
+    plot_line(
+        signal_data=raw_data, sqi=raw_sqi, example=random_noisy_example, name="raw"
+    )
+    plot_line(
+        signal_data=downsampled,
+        sqi=downsampled_sqi,
+        example=random_noisy_example,
+        name="downsampled",
+    )
+    plt.title(random_noisy_example)
+    plt.legend()
+    plt.show()
