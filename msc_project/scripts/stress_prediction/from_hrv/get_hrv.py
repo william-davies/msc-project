5f8dd66cebd92715986837ef641aa33b14afe826
william-davies <daviwt123@gmail.com>
2021-10-01 17:59:41 +0100

    Get hrv from windowed signal

--- msc_project/scripts/stress_prediction/from_hrv/get_hrv.py
+++ msc_project/scripts/stress_prediction/from_hrv/get_hrv.py
@@ -2,6 +2,8 @@
 Get HRV features that will be used for stress prediction. I'm using a sliding window this time, so I made a different file
 to `hrv/get_hrv`, where I didn't use a sliding window and I also had input signals from both Infiniti and Empatica.
 """
+import os
+
 import wandb
 
 from msc_project.constants import (
@@ -9,31 +11,14 @@ from msc_project.constants import (
     DENOISING_AUTOENCODER_PROJECT_NAME,
     PREPROCESSED_DATA_ARTIFACT,
 )
+from msc_project.scripts.hrv.get_hrv import get_hrv
+from msc_project.scripts.hrv.get_whole_signal_hrv import add_temp_file_to_artifact
 from msc_project.scripts.utils import get_artifact_dataframe
 
 
-def get_preprocessed_data_artifact(dae_denoised_artifact):
-    """
-    Get preprocessed data artifact that contains the signal that was denoised to create the `dae_denoised_artifact`.
-    :param dae_denoised_artifact:
-    :return:
-    """
-    run = dae_denoised_artifact.logged_by()
-    artifacts = run.used_artifacts()
-    preprocessed_data_artifact = [
-        artifact
-        for artifact in artifacts
-        if artifact.type == PREPROCESSED_DATA_ARTIFACT
-    ]
-    assert len(preprocessed_data_artifact) == 1
-    preprocessed_data_artifact = preprocessed_data_artifact[0]
-    return preprocessed_data_artifact
-
-
 if __name__ == "__main__":
-    dae_denoised_data_artifact_name: str = (
-        f"{DENOISING_AUTOENCODER_PROJECT_NAME}/Inf_merged_signal:v0"
-    )
+    windowed_data_artifact_name: str = "windowed:v0"
+    upload_artifact: bool = True
 
     run = wandb.init(
         project=STRESS_PREDICTION_PROJECT_NAME,
@@ -41,17 +26,56 @@ if __name__ == "__main__":
         save_code=True,
     )
 
-    dae_denoised_artifact = run.use_artifact(dae_denoised_data_artifact_name)
-
-    # load Infiniti raw data, just downsampled, traditional preprocessing, DAE denoised
-    inf_raw_data = get_artifact_dataframe(
+    # load raw, just downsampled, traditional preprocessed, DAE signal
+    raw_signal = get_artifact_dataframe(
         run=run,
-        artifact_or_name="Inf_preprocessed_data:v4",
-        pkl_filename="windowed_raw_data.pkl",
+        artifact_or_name=windowed_data_artifact_name,
+        pkl_filename="raw_signal.pkl",
+    )
+    just_downsampled_signal = get_artifact_dataframe(
+        run=run,
+        artifact_or_name=windowed_data_artifact_name,
+        pkl_filename="just_downsampled_signal.pkl",
+    )
+    traditional_preprocessed_signal = get_artifact_dataframe(
+        run=run,
+        artifact_or_name=windowed_data_artifact_name,
+        pkl_filename="traditional_preprocessed_signal.pkl",
+    )
+    dae_denoised_signal = get_artifact_dataframe(
+        run=run,
+        artifact_or_name=windowed_data_artifact_name,
+        pkl_filename="dae_denoised_signal.pkl",
     )
 
-    # sliding window
-
     # compute HRV features
+    print("raw_hrv")
+    raw_hrv = get_hrv(signal_data=raw_signal)
+    print("just_downsampled_hrv")
+    just_downsampled_hrv = get_hrv(signal_data=just_downsampled_signal)
+    print("traditional_preprocessed_hrv")
+    traditional_preprocessed_hrv = get_hrv(signal_data=traditional_preprocessed_signal)
+    print("dae_denoised_hrv")
+    dae_denoised_hrv = get_hrv(signal_data=dae_denoised_signal)
 
     # save HRV features
+    if upload_artifact:
+        artifact = wandb.Artifact(name="hrv", type="get_hrv")
+        add_temp_file_to_artifact(
+            artifact=artifact, fp="raw_signal_hrv.pkl", df=raw_hrv
+        )
+        add_temp_file_to_artifact(
+            artifact=artifact,
+            fp="just_downsampled_signal_hrv.pkl",
+            df=just_downsampled_hrv,
+        )
+        add_temp_file_to_artifact(
+            artifact=artifact,
+            fp="traditional_preprocessed_signal_hrv.pkl",
+            df=traditional_preprocessed_hrv,
+        )
+        add_temp_file_to_artifact(
+            artifact=artifact, fp="dae_denoised_signal_hrv.pkl", df=dae_denoised_hrv
+        )
+        run.log_artifact(artifact)
+    run.finish()
