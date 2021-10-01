40bbe5113e7b2cbcf959d2dacc45bae0c9ab0327
william-davies <daviwt123@gmail.com>
2021-10-01 21:01:23 +0100

    Standardize hrv features

--- msc_project/scripts/stress_prediction/from_hrv/get_hrv_features.py
+++ msc_project/scripts/stress_prediction/from_hrv/get_hrv_features.py
@@ -1,17 +1,39 @@
 """
 Get data ready for model. We need (`num_examples`, `num_features`) DataFrame for x.
-We need (`num_examples`, 1) DataFrame for y.
 """
+import pandas as pd
+
 import wandb
 from msc_project.constants import STRESS_PREDICTION_PROJECT_NAME
 from msc_project.scripts.hrv.get_rmse import metrics_of_interest
+from msc_project.scripts.hrv.get_whole_signal_hrv import add_temp_file_to_artifact
 from msc_project.scripts.stress_prediction.from_signal_itself.preprocess_data import (
     get_labels,
 )
 from msc_project.scripts.utils import get_artifact_dataframe
 
+
+def standardize_hrv_features(hrv_features: pd.DataFrame):
+    """
+    Following Jade.
+    :return:
+    """
+    standardized = hrv_features.copy()
+    for participant_idx, participant_df in hrv_features.groupby(
+        axis=0, level="participant"
+    ):
+        standardized_participant = participant_df.copy()
+        baseline = participant_df.loc[(participant_idx, "r1", "bvp", 60)]
+        standardized_participant.loc[
+            :, standardized_participant.columns != "pnn50"
+        ] /= baseline[baseline.keys() != "pnn50"]
+        standardized_participant.loc[:, "pnn50"] -= baseline["pnn50"]
+        standardized.loc[participant_idx] = standardized_participant
+
+
 if __name__ == "__main__":
     heartpy_output_artifact_name: str = "hrv:v0"
+    upload_artifact: bool = True
 
     run = wandb.init(
         project=STRESS_PREDICTION_PROJECT_NAME,
@@ -64,5 +86,32 @@ if __name__ == "__main__":
     ]:
         assert raw_signal_features.columns.equals(df.columns)
 
-    # get binary labels
-    labels = get_labels(windowed_data=raw_signal_features).T
+    raw_signal_features = raw_signal_features.T
+    just_downsampled_signal_features = just_downsampled_signal_features.T
+    traditional_preprocessed_signal_features = (
+        traditional_preprocessed_signal_features.T
+    )
+    dae_denoised_signal_features = dae_denoised_signal_features.T
+
+    if upload_artifact:
+        artifact = wandb.Artifact(name="hrv_features", type="get_hrv")
+        add_temp_file_to_artifact(
+            artifact=artifact, fp="raw_signal.pkl", df=raw_signal_features
+        )
+        add_temp_file_to_artifact(
+            artifact=artifact,
+            fp="just_downsampled_signal.pkl",
+            df=just_downsampled_signal_features,
+        )
+        add_temp_file_to_artifact(
+            artifact=artifact,
+            fp="traditional_preprocessed_signal.pkl",
+            df=traditional_preprocessed_signal_features,
+        )
+        add_temp_file_to_artifact(
+            artifact=artifact,
+            fp="dae_denoised_signal.pkl",
+            df=dae_denoised_signal_features,
+        )
+        run.log_artifact(artifact)
+    run.finish()
