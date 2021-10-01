b09dbf243f3359389155fe49ccded491e7cd235c
william-davies <daviwt123@gmail.com>
2021-10-01 18:37:30 +0100

    Filter out hrv metrics of interest

---
+++ msc_project/scripts/stress_prediction/from_hrv/make_dataset.py
@@ -0,0 +1,60 @@
+import wandb
+from msc_project.constants import STRESS_PREDICTION_PROJECT_NAME
+from msc_project.scripts.hrv.get_rmse import metrics_of_interest
+from msc_project.scripts.utils import get_artifact_dataframe
+
+if __name__ == "__main__":
+    heartpy_output_artifact_name: str = "hrv:v0"
+
+    run = wandb.init(
+        project=STRESS_PREDICTION_PROJECT_NAME,
+        job_type="get_dataset",
+        save_code=True,
+    )
+
+    # load raw, just downsampled, traditional preprocessed, DAE heartpy outputs
+    raw_signal_heartpy_output = get_artifact_dataframe(
+        run=run,
+        artifact_or_name=heartpy_output_artifact_name,
+        pkl_filename="raw_signal_hrv.pkl",
+    )
+    just_downsampled_signal_heartpy_output = get_artifact_dataframe(
+        run=run,
+        artifact_or_name=heartpy_output_artifact_name,
+        pkl_filename="just_downsampled_signal_hrv.pkl",
+    )
+    traditional_preprocessed_signal_heartpy_output = get_artifact_dataframe(
+        run=run,
+        artifact_or_name=heartpy_output_artifact_name,
+        pkl_filename="traditional_preprocessed_signal_hrv.pkl",
+    )
+    dae_denoised_signal_heartpy_output = get_artifact_dataframe(
+        run=run,
+        artifact_or_name=heartpy_output_artifact_name,
+        pkl_filename="dae_denoised_signal_hrv.pkl",
+    )
+
+    # filter out HRV features for classifier
+    raw_signal_features = raw_signal_heartpy_output.loc[metrics_of_interest]
+    just_downsampled_signal_features = just_downsampled_signal_heartpy_output.loc[
+        metrics_of_interest
+    ]
+    traditional_preprocessed_signal_features = (
+        traditional_preprocessed_signal_heartpy_output.loc[metrics_of_interest]
+    )
+    dae_denoised_signal_features = dae_denoised_signal_heartpy_output.loc[
+        metrics_of_interest
+    ]
+
+    # sort windows
+    dae_denoised_signal_features = dae_denoised_signal_features[
+        raw_signal_features.columns
+    ]
+    for df in [
+        just_downsampled_signal_features,
+        traditional_preprocessed_signal_features,
+        dae_denoised_signal_features,
+    ]:
+        assert raw_signal_features.columns.equals(df.columns)
+
+    # get binary labels
