8740c1eef53a17a3dd8b6094c451aeebc5f3a7f7
william-davies <daviwt123@gmail.com>
2021-10-02 03:17:09 +0100

    get_math_difficulty

--- msc_project/scripts/stress_prediction/from_hrv/get_hrv_features.py
+++ msc_project/scripts/stress_prediction/from_hrv/get_hrv_features.py
@@ -1,6 +1,8 @@
 """
 Get data ready for model. We need (`num_examples`, `num_features`) DataFrame for x.
 """
+import re
+
 import pandas as pd
 
 import wandb
@@ -33,6 +35,41 @@ def standardize_hrv_features(hrv_features: pd.DataFrame):
         standardized.index.get_level_values(level="treatment_label") != "r1"
     ]
     return non_baseline
+
+
+def change_treatment_labels(df):
+    def get_math_difficulty(treatment_labels, index: int) -> str:
+        """
+        Get the difficulty of the math treatment at `index`.
+        :param treatment_labels:
+        :param index: \in {2,4}
+        :return: \in {easy, hard}
+        """
+        math_treatment_pattern = f"m{index}_(?P<difficulty>\w{{4}})"
+
+        math_label = [
+            treatment
+            for treatment in treatment_labels
+            if treatment.startswith(f"m{index}_")
+        ]
+        assert len(math_label) == 1
+        math_label = math_label[0]
+        difficulty = (
+            re.compile(math_treatment_pattern).match(math_label).group("difficulty")
+        )
+        return difficulty
+
+    for idx, treatment_df in df.groupby(axis=0, level="participant"):
+        mycopy = treatment_df.copy()
+        treatment_labels = treatment_df.index.get_level_values(
+            level="treatment_label"
+        ).unique()
+        m2_label = [
+            treatment for treatment in treatment_labels if treatment.startswith("m2_")
+        ]
+        assert len(m2_label) == 1
+        m2_label = m2_label[0]
+        # m2_difficulty =
 
 
 if __name__ == "__main__":
