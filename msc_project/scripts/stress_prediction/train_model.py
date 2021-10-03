745aefee7f1f420d61d3379fd8a945f85acea625
william-davies <daviwt123@gmail.com>
2021-10-03 21:31:25 +0100

    make standardize_data helper function

--- msc_project/scripts/stress_prediction/train_model.py
+++ msc_project/scripts/stress_prediction/train_model.py
@@ -1,6 +1,7 @@
 """
 Train a Gaussian Naive Bayes classifier to predict binary stress label from input HRV features.
 """
+import pandas as pd
 import wandb
 from sklearn.naive_bayes import GaussianNB
 from sklearn import preprocessing
@@ -8,6 +9,13 @@ from sklearn import preprocessing
 from msc_project.constants import STRESS_PREDICTION_PROJECT_NAME
 from msc_project.scripts.dataset_preparer import get_test_participants, train_test_split
 from msc_project.scripts.utils import get_artifact_dataframe
+
+
+def standardize_data(
+    data: pd.DataFrame, mean: pd.Series, std: pd.Series
+) -> pd.DataFrame:
+    return (data - mean) / std
+
 
 if __name__ == "__main__":
     dataset_artifact_name = "complete_dataset:v1"
@@ -34,8 +42,9 @@ if __name__ == "__main__":
     test_participants = get_test_participants(test_size=0.3)
     train, test = train_test_split(data=hrv_input, test_participants=test_participants)
 
-    # normalize features
+    # standardize features
     # use train mean and std because that's what Jade did
-    scaler = preprocessing.StandardScaler().fit(train)
     train_mean = train.mean(axis=0)
     train_std = train.std(axis=0, ddof=0)  # population standard deviation. like sklearn
+    standardized_train = standardize_data(data=train, mean=train_mean, std=train_std)
+    standardized_test = standardize_data(data=test, mean=train_mean, std=train_std)
