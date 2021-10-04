20f6893fa417d7fa39574359f5c03d7536d1621c
william-davies <daviwt123@gmail.com>
2021-10-04 05:55:20 +0100

    Test out macro f1

--- msc_project/scripts/stress_prediction/from_hrv/testing_ground.py
+++ msc_project/scripts/stress_prediction/from_hrv/testing_ground.py
@@ -1,18 +1,12 @@
+from sklearn.metrics import f1_score
 import numpy as np
-from sklearn.model_selection import LeaveOneGroupOut
 
-X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
-y = np.array([1, 2, 3, 4])
-groups = np.array([1, 1, 2, 2])
-logo = LeaveOneGroupOut()
-logo.get_n_splits(X, y, groups)
+y_true = [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0]
+y_pred = [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+macro_f1_score = f1_score(y_true, y_pred, average="macro")
+print(f"macro_f1_score: {macro_f1_score}")
 
-logo.get_n_splits(groups=groups)  # 'groups' is always required
-
-print(logo)
-
-for train_index, test_index in logo.split(X, y, groups):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    print(X_train, X_test, y_train, y_test)
+positive_f1_score = f1_score(y_true, y_pred, pos_label=1)
+negative_f1_score = f1_score(y_true, y_pred, pos_label=0)
+mean = np.mean((positive_f1_score, negative_f1_score))
+print(f"mean: {mean}")
