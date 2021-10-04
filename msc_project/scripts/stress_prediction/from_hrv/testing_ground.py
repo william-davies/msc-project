b379e41f66e8eb2e4c57b80098dcddb793736ae1
william-davies <daviwt123@gmail.com>
2021-10-04 15:25:40 +0100

    LOGO seems to go in ascending group order

--- msc_project/scripts/stress_prediction/from_hrv/testing_ground.py
+++ msc_project/scripts/stress_prediction/from_hrv/testing_ground.py
@@ -1,12 +1,29 @@
-from sklearn.metrics import f1_score
 import numpy as np
+from sklearn.model_selection import LeaveOneGroupOut
 
-y_true = [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0]
-y_pred = [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0]
-macro_f1_score = f1_score(y_true, y_pred, average="macro")
-print(f"macro_f1_score: {macro_f1_score}")
+num_examples = 16
+num_features = 2
+group_size = 2
 
-positive_f1_score = f1_score(y_true, y_pred, pos_label=1)
-negative_f1_score = f1_score(y_true, y_pred, pos_label=0)
-mean = np.mean((positive_f1_score, negative_f1_score))
-print(f"mean: {mean}")
+groups = np.arange(num_examples)
+groups = np.repeat(groups, group_size)
+
+groups = np.random.choice(a=8, size=num_examples, replace=True)
+
+X = np.arange(num_examples * num_features).reshape((num_examples, num_features))
+y = np.arange(num_examples)
+logo = LeaveOneGroupOut()
+logo.get_n_splits(X, y, groups)
+
+logo.get_n_splits(groups=groups)  # 'groups' is always required
+
+# print(X)
+# print(y)
+print(groups)
+
+for train_index, test_index in logo.split(X, y, groups):
+    # print("TRAIN:", train_index, "TEST:", test_index)
+    print(f"TEST GROUPS: {groups[test_index]}")
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    # print(X_train, X_test, y_train, y_test)
