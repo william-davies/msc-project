5a1a03d64fab8ec0ac3e0eab64347d209ab2a673
william-davies <daviwt123@gmail.com>
2021-10-04 05:46:24 +0100

    Evaluate accuracy

--- msc_project/scripts/stress_prediction/train_model.py
+++ msc_project/scripts/stress_prediction/train_model.py
@@ -3,12 +3,16 @@ Train a Gaussian Naive Bayes classifier to predict binary stress label from inpu
 """
 import pandas as pd
 import wandb
+from sklearn.dummy import DummyClassifier
+from sklearn.model_selection import cross_validate
 from sklearn.naive_bayes import GaussianNB
 from sklearn import preprocessing
+from sklearn.metrics import accuracy_score, matthews_corrcoef
 
 from msc_project.constants import STRESS_PREDICTION_PROJECT_NAME
 from msc_project.scripts.dataset_preparer import get_test_participants, train_test_split
 from msc_project.scripts.utils import get_artifact_dataframe
+from sklearn.pipeline import make_pipeline
 
 
 def standardize_data(
@@ -27,12 +31,12 @@ if __name__ == "__main__":
     )
 
     # load dataset
-    hrv_input = get_artifact_dataframe(
+    X = get_artifact_dataframe(
         run=run,
         artifact_or_name=dataset_artifact_name,
         pkl_filename="hrv_features.pkl",
     )
-    labels = get_artifact_dataframe(
+    y = get_artifact_dataframe(
         run=run,
         artifact_or_name=dataset_artifact_name,
         pkl_filename="stress_labels.pkl",
@@ -40,21 +44,33 @@ if __name__ == "__main__":
 
     # split into train test
     test_participants = get_test_participants(test_size=0.3)
-    train_X, test_X = train_test_split(
-        data=hrv_input, test_participants=test_participants
-    )
-    train_labels, test_labels = train_test_split(
-        data=labels.squeeze(), test_participants=test_participants
+    X_train, X_test = train_test_split(data=X, test_participants=test_participants)
+    y_train, y_test = train_test_split(
+        data=y.squeeze(), test_participants=test_participants
     )
 
     # standardize features
     # use train mean and std because that's what Jade did
-    train_mean = train_X.mean(axis=0)
-    train_std = train_X.std(
+    train_mean = X_train.mean(axis=0)
+    train_std = X_train.std(
         axis=0, ddof=0
     )  # population standard deviation. like sklearn
-    train_X = standardize_data(data=train_X, mean=train_mean, std=train_std)
-    test_X = standardize_data(data=test_X, mean=train_mean, std=train_std)
+    X_train = standardize_data(data=X_train, mean=train_mean, std=train_std)
+    X_test = standardize_data(data=X_test, mean=train_mean, std=train_std)
 
+    # evaluate model
     gnb = GaussianNB()
-    gnb.fit(train_X, train_labels)
+    gnb.fit(X_train, y_train)
+    mymodel_acc = gnb.score(X_test, y_test)
+
+    dummy_clf = DummyClassifier(strategy="most_frequent", random_state=0)
+    dummy_clf.fit(X_train, y_train)
+    dummy_acc = dummy_clf.score(X_test, y_test)
+
+    print(f"my model score: {mymodel_acc}")
+    print(f"dummy score: {dummy_acc}")
+
+    # LOSO-CV
+    gnb = make_pipeline(preprocessing.StandardScaler(), GaussianNB())
+    scoring = ["accuracy", "f1_macro", matthews_corrcoef]
+    scores = cross_validate(gnb, X, y, scoring=scoring, cv=5, return_train_score=True)
