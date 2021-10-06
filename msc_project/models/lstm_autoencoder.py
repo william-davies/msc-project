008e83cb529b7560fb9efbc6a04e357b0b60570e
william-davies <daviwt123@gmail.com>
2021-10-06 16:46:32 +0100

    delete stale lstm code

--- msc_project/models/lstm_autoencoder.py
+++ msc_project/models/lstm_autoencoder.py
@@ -13,37 +13,6 @@ from tensorflow.keras.layers import (
 )
 
 
-# def create_autoencoder(config: Dict):
-#     """
-#
-#     :param config:
-#     :return:
-#     """
-#     bottleneck_size = 8
-#     autoencoder = tf.keras.Sequential(
-#         [
-#             # encoder
-#             keras.layers.LSTM(
-#                 units=bottleneck_size,
-#                 activation="tanh",
-#                 input_shape=(config["timeseries_length"], 1),
-#             ),
-#             RepeatVector(config["timeseries_length"]),
-#             # decoder
-#             keras.layers.LSTM(
-#                 units=bottleneck_size, activation="tanh", return_sequences=True
-#             ),
-#             keras.layers.TimeDistributed(keras.layers.Dense(1, activation="sigmoid")),
-#         ]
-#     )
-#
-#     autoencoder.compile(
-#         optimizer=config["optimizer"], loss=config["loss"], metrics=[config["metric"]]
-#     )
-#
-#     return autoencoder
-
-
 def create_autoencoder(config: Dict):
     """
 
@@ -97,4 +66,4 @@ if __name__ == "__main__":
     }
     autoencoder = create_autoencoder(metadata)
     plot_model(autoencoder, show_shapes=True, to_file="blstm_autoencoder.png")
-    print(autoencoder.summary())
+    print(autoencoder.fitted_model_summary())
