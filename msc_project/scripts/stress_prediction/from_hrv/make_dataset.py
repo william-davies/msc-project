ce5a06ddc7569c197e2c8b988219e0ed5832dcae
william-davies <daviwt123@gmail.com>
2021-10-09 14:41:59 +0100

    make_dataset now supports different sheets

--- msc_project/scripts/stress_prediction/from_hrv/make_dataset.py
+++ msc_project/scripts/stress_prediction/from_hrv/make_dataset.py
@@ -1,26 +1,31 @@
 """
-Load HRV features (input) and labels (target). Filter by clean signals.
+Load HRV features (input) and labels (target). Filter by clean signals. Filter features to use.
+Parameters
+- hrv features to use
+- participants to exclude
 """
 import os
-from typing import List
+from typing import List, Dict
 
 import pandas as pd
 import wandb
 from msc_project.constants import STRESS_PREDICTION_PROJECT_NAME
-from msc_project.scripts.hrv.get_rmse import get_clean_signal_indexes
-from msc_project.scripts.hrv.get_whole_signal_hrv import add_temp_file_to_artifact
+from msc_project.scripts.hrv.get_rmse import (
+    get_clean_signal_indexes,
+    metrics_of_interest,
+)
+from msc_project.scripts.hrv.utils import add_temp_file_to_artifact
 from msc_project.scripts.stress_prediction.from_hrv.get_hrv_features import (
     change_treatment_labels,
     get_non_baseline_windows,
 )
-from msc_project.scripts.utils import get_artifact_dataframe
-
-
-def get_single_input_artifact(run):
-    artifact = run.used_artifacts()
-    assert len(artifact) == 1
-    artifact = artifact[0]
-    return artifact
+from msc_project.scripts.stress_prediction.from_hrv.get_preprocessed_data import (
+    get_sheet_name_prefix,
+)
+from msc_project.scripts.stress_prediction.from_hrv.train_model import (
+    preprocessing_methods,
+)
+from msc_project.scripts.utils import get_artifact_dataframe, get_single_input_artifact
 
 
 def get_windowed_artifact(hrv_features_artifact):
@@ -31,30 +36,41 @@ def get_windowed_artifact(hrv_features_artifact):
     return windowed_artifact
 
 
-def filter_data(data, excluded_participants: List[str]) -> pd.DataFrame:
+def filter_labels(labels: pd.DataFrame, config: Dict) -> pd.DataFrame:
     """
     I do different data filtering as I do experiments. Put them all in here so it's cleaner.
-    :param data:
+    :param labels:
     :return:
     """
-    return data.drop(index=excluded_participants)
+    return labels.drop(index=config["excluded_participants"])
+
+
+def filter_hrv_features(hrv_features: pd.DataFrame, config: Dict) -> pd.DataFrame:
+    """
+    I do different data filtering as I do experiments. Put them all in here so it's cleaner.
+    :param hrv_features:
+    :return:
+    """
+    return hrv_features.drop(index=config["excluded_participants"]).loc[
+        :, config["included_features"]
+    ]
 
 
 if __name__ == "__main__":
-    hrv_features_artifact_name: str = "hrv_features:v4"
-    labels_artifact_name: str = "labels:v0"
-    dataset_names: List[str] = [
-        "raw",
-        "just_downsampled",
-        "traditional_preprocessed",
-        "dae_denoised",
-    ]
+    hrv_features_artifact_name: str = "EmLBVP_hrv_features:v0"
+    labels_artifact_name: str = "EmLBVP_labels:v0"
     config = {
         "noise_tolerance": 1,
-        "excluded_participants": ["0725135216P4_608", "0726094551P5_609"],
+        "excluded_participants": ["0726114041P6_609"],
+        "included_features": ["bpm", "sdnn", "rmssd", "pnn50", "lf", "hf", "lf/hf"],
     }
     notes = ""
     upload_to_wandb: bool = True
+
+    assert get_sheet_name_prefix(hrv_features_artifact_name) == get_sheet_name_prefix(
+        labels_artifact_name
+    )
+    sheet_name = get_sheet_name_prefix(hrv_features_artifact_name)
 
     run = wandb.init(
         project=STRESS_PREDICTION_PROJECT_NAME,
@@ -88,40 +104,42 @@ if __name__ == "__main__":
         artifact_or_name=labels_artifact_name,
         pkl_filename="labels.pkl",
     )
-    filtered_labels = filter_data(
-        data=labels, excluded_participants=config["excluded_participants"]
-    )
+    filtered_labels = filter_labels(labels=labels, config=config)
 
-    artifact = wandb.Artifact(
-        name="complete_dataset",
+    complete_dataset_artifact = wandb.Artifact(
+        name=f"{sheet_name}_complete_dataset",
         type="get_dataset",
         metadata=config,
         description=notes,
     )
 
     # load HRV features
-    for dataset_name in dataset_names:
+    for preprocessing_method in preprocessing_methods:
         hrv_features = get_artifact_dataframe(
             run=run,
             artifact_or_name=hrv_features_artifact_name,
-            pkl_filename=os.path.join("changed_label", f"{dataset_name}_signal.pkl"),
+            pkl_filename=os.path.join(
+                "changed_label", f"{preprocessing_method}_signal.pkl"
+            ),
         )
 
-        filtered_hrv_features = filter_data(
-            data=hrv_features, excluded_participants=config["excluded_participants"]
+        filtered_hrv_features = filter_hrv_features(
+            hrv_features=hrv_features, config=config
         )
         # check input and labels are in same order
         assert filtered_hrv_features.index.equals(filtered_labels.index)
         add_temp_file_to_artifact(
-            artifact=artifact,
-            fp=f"{dataset_name}_signal_hrv_features.pkl",
+            artifact=complete_dataset_artifact,
+            fp=f"{preprocessing_method}_signal_hrv_features.pkl",
             df=filtered_hrv_features,
         )
 
     # upload to wandb
     if upload_to_wandb:
         add_temp_file_to_artifact(
-            artifact=artifact, fp="stress_labels.pkl", df=filtered_labels
+            artifact=complete_dataset_artifact,
+            fp="stress_labels.pkl",
+            df=filtered_labels,
         )
-        run.log_artifact(artifact)
+        run.log_artifact(complete_dataset_artifact)
     run.finish()
