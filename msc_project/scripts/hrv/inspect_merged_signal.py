59afd97cbdc229bd12d8b735938cea0e5bbd73f7
william-davies <daviwt123@gmail.com>
2021-10-23 23:02:44 +0100

    Change merged signal format to make it stand out more

--- msc_project/scripts/hrv/inspect_merged_signal.py
+++ msc_project/scripts/hrv/inspect_merged_signal.py
@@ -20,14 +20,15 @@ from msc_project.scripts.evaluate_autoencoder import (
     download_artifact_model,
     get_reconstructed_df,
 )
+from msc_project.scripts.hrv.get_rmse import get_noisy_proportions
 
 from msc_project.scripts.plot_raw_data import plot_signal
-from msc_project.scripts.utils import get_artifact_dataframe
+from msc_project.scripts.utils import get_committed_artifact_dataframe
 
 if __name__ == "__main__":
-    preprocessed_data_artifact_name: str = "EmLBVP_preprocessed_data:v7"
-    model_artifact_name = "trained_on_EmLBVP:v3"
-    merged_signal_artifact_name: str = "EmLBVP_merged_signal:v2"
+    preprocessed_data_artifact_name: str = "EmLBVP_preprocessed_data:v8"
+    model_artifact_name = "trained_on_EmLBVP:v6"
+    merged_signal_artifact_name: str = "EmLBVP_merged_signal:v3"
     data_name: str = "only_downsampled"
     config = {"data_name": data_name}
 
@@ -39,11 +40,20 @@ if __name__ == "__main__":
     )
 
     # 1. get reconstructed windows
-    only_downsampled_data = get_artifact_dataframe(
+    only_downsampled_data = get_committed_artifact_dataframe(
         run=run,
         artifact_or_name=preprocessed_data_artifact_name,
         pkl_filename=os.path.join("windowed", f"{data_name}_data.pkl"),
     )
+    noisy_mask = get_committed_artifact_dataframe(
+        run=run,
+        artifact_or_name=preprocessed_data_artifact_name,
+        pkl_filename=os.path.join("windowed", f"noisy_mask.pkl"),
+    )
+    noisy_proportions = get_noisy_proportions(noisy_mask=noisy_mask)
+    noise_per_treatment = noisy_proportions.groupby(
+        level=["participant", "treatment_label"]
+    ).mean()
 
     autoencoder = download_artifact_model(run=run, artifact_or_name=model_artifact_name)
     reconstructed_windows = get_reconstructed_df(
@@ -55,14 +65,14 @@ if __name__ == "__main__":
     )
 
     # 2. load merged signal
-    merged_signal = get_artifact_dataframe(
+    merged_signal = get_committed_artifact_dataframe(
         run=run,
         artifact_or_name=merged_signal_artifact_name,
         pkl_filename="merged_signal.pkl",
     )
 
     # get model input
-    not_windowed_downsampled_data = get_artifact_dataframe(
+    not_windowed_downsampled_data = get_committed_artifact_dataframe(
         run=run,
         artifact_or_name=preprocessed_data_artifact_name,
         pkl_filename=os.path.join("not_windowed", f"{data_name}_data.pkl"),
@@ -75,15 +85,24 @@ if __name__ == "__main__":
         treatment_windows + 1
     )  # so that they're not all overlapping and illegible
     plt.figure()
+    scaling_factor = 4
     for idx in treatment_windows.iloc[:, :]:
         window = treatment_windows[idx]
         # this is very opaque code. basically plots all the reconstructed windows in different locations so that
         # you can actually look at them
         # %8 makes this periodic
-        offset = ((idx - SECONDS_IN_MINUTE) % 8) / 1.5
+        offset = ((idx - SECONDS_IN_MINUTE) % 8) / 1.5 + scaling_factor
         plot_signal(signal=window + offset)
-    plot_signal(signal=merged_signal[arbitrary_example])
+    plot_signal(
+        signal=scaling_factor * merged_signal[arbitrary_example],
+        color="k",
+        linestyle="--",
+    )
     plt.title(arbitrary_example)
+    plt.ylabel("PPG (a.u)")
+    plt.xlabel("Time in session (s)")
+    ax = plt.gca()
+    ax.axes.yaxis.set_ticks([])
     plt.show()
 
     plt.figure()
